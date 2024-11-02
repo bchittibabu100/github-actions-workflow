ac26a5e2352cf72d8c59fb0bbb4511357df29c0c
Chittibabu Boya <bchittibabu100@gmail.com>
2024-11-02 09:40:32 -0700

    Update hashicorp_vault_install_and_uninstall.sh
    Updated one

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+
 backup_path=/mnt/vault_backup
 RETENTION_DAYS=14
 usage() {
-  echo "Usage: $0 <install/uninstall> [version]"
-  echo "For install: $0 install <vault_version>"
-  echo "For uninstall: $0 uninstall"
+  echo "Usage: $0 <install/uninstall> <dev/stage/prod> [version]"
+  echo "For install: $0 install <instance> <vault_version>"
+  echo "For uninstall: $0 uninstall <instance>"
   exit 1
 }
 
-
 install_vault() {
-  local VAULT_VERSION=$1
+  local INSTANCE=$1
+  local VAULT_VERSION=$2
   local VAULT_URL="https://repo1.xyz.com:443/artifactory/hashicorp-releases-cache/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip"
-  echo "Downloading Vault version $VAULT_VERSION..."
+  local CONFIG_DIR="/etc/vault.d/${INSTANCE}"
+  local DATA_DIR="/opt/data/vault/${INSTANCE}"
+  local SERVICE_NAME="vault-${INSTANCE}.service"
+
+  echo "Downloading Vault version $VAULT_VERSION for $INSTANCE instance..."
   curl -O $VAULT_URL
 
   if [ $? -ne 0 ]; then
@@ -25,12 +30,12 @@ install_vault() {
   echo "Installing Vault..."
   sudo mv vault /usr/local/bin/
 
-  echo "Setting up Vault directories and permissions..."
-  sudo mkdir -p /opt/data/vault
-  sudo mkdir -p /etc/vault.d
-  sudo mkdir -p $backup_path
-  sudo mkdir -p /opt/vault_cron_logs
-  sudo touch /etc/vault.d/vault.hcl
+  echo "Setting up Vault directories and permissions for $INSTANCE instance..."
+  sudo mkdir -p $DATA_DIR
+  sudo mkdir -p $CONFIG_DIR
+  sudo mkdir -p $backup_path/$INSTANCE
+  sudo mkdir -p /opt/vault_cron_logs/$INSTANCE
+  sudo touch $CONFIG_DIR/vault.hcl
 
   echo "Creating vault user..."
   if ! id -u vault &>/dev/null; then
@@ -38,38 +43,38 @@ install_vault() {
   fi
 
   sudo chmod +x /usr/local/bin/vault
-  sudo chown -R vault:vault /etc/vault.d
-  sudo chmod -R 750 /etc/vault.d
-  sudo chown -R vault:vault /opt/data/vault
-  sudo chmod -R 750 /opt/data/vault $backup_path /opt/vault_cron_logs
+  sudo chown -R vault:vault $CONFIG_DIR
+  sudo chmod -R 750 $CONFIG_DIR
+  sudo chown -R vault:vault $DATA_DIR
+  sudo chmod -R 750 $DATA_DIR $backup_path/$INSTANCE /opt/vault_cron_logs/$INSTANCE
 
-  echo "Creating /etc/vault.d/vault.hcl configuration file..."
-  sudo bash -c 'cat > /etc/vault.d/vault.hcl <<EOF
+  echo "Creating $CONFIG_DIR/vault.hcl configuration file for $INSTANCE instance..."
+  sudo bash -c "cat > $CONFIG_DIR/vault.hcl <<EOF
 # Storage backend
 disable_mlock = true
 ui = true
-cluster_addr  = "http://127.0.0.1:8201"
-api_addr      = "http://127.0.0.1:8200"
-storage "raft" {
-  path = "/opt/data/vault"
-  node_id = "node_1"
+cluster_addr  = \"http://127.0.0.1:82${INSTANCE}1\"
+api_addr      = \"http://127.0.0.1:82${INSTANCE}0\"
+storage \"raft\" {
+  path = \"$DATA_DIR\"
+  node_id = \"${INSTANCE}_node\"
 }
-listener "tcp" {
-  address     = "0.0.0.0:8200"
+listener \"tcp\" {
+  address     = \"0.0.0.0:82${INSTANCE}0\"
   tls_disable = 1
 }
-EOF'
+EOF"
 
-  sudo chown vault:vault /etc/vault.d/vault.hcl
+  sudo chown vault:vault $CONFIG_DIR/vault.hcl
 
-  echo "Setting up systemd service for vault..."
-  sudo bash -c 'cat > /etc/systemd/system/vault.service <<EOF
+  echo "Setting up systemd service for $INSTANCE instance..."
+  sudo bash -c "cat > /etc/systemd/system/$SERVICE_NAME <<EOF
 [Unit]
-Description="HashiCorp Vault - A tool for managing secrets"
+Description=\"HashiCorp Vault ($INSTANCE instance) - A tool for managing secrets\"
 Documentation=https://github.com/xyz
 Requires=network-online.target
 After=network-online.target
-ConditionFileNotEmpty=/etc/vault.d/vault.hcl
+ConditionFileNotEmpty=$CONFIG_DIR/vault.hcl
 StartLimitIntervalSec=60
 StartLimitBurst=3
 
@@ -85,8 +90,8 @@ AmbientCapabilities=CAP_IPC_LOCK
 Capabilities=CAP_IPC_LOCK+ep
 CapabilityBoundingSet=CAP_SYSLOG CAP_IPC_LOCK
 NoNewPrivileges=yes
-ExecStart=/usr/local/bin/vault server -config=/etc/vault.d/vault.hcl
-ExecReload=/bin/kill --signal HUP $MAINPID
+ExecStart=/usr/local/bin/vault server -config=$CONFIG_DIR/vault.hcl
+ExecReload=/bin/kill --signal HUP \$MAINPID
 KillMode=process
 KillSignal=SIGINT
 Restart=on-failure
@@ -99,95 +104,97 @@ LimitNOFILE=65536
 LimitMEMLOCK=infinity
 [Install]
 WantedBy=multi-user.target
-EOF'
+EOF"
 
-  sudo systemctl enable vault.service
-  sudo systemctl start vault.service
+  sudo systemctl enable $SERVICE_NAME
+  sudo systemctl start $SERVICE_NAME
 
-  echo "Vault service with version:${VAULT_VERSION} installed:"
-  echo "Please wait..."
+  echo "Vault service for $INSTANCE instance installed with version: ${VAULT_VERSION}."
   sleep 10
   vault --version
   rm -f vault_${VAULT_VERSION}_linux_amd64.zip
 
-  export VAULT_ADDR=http://127.0.0.1:8200
-  echo "Initializing Vault and storing keys..."
-  sudo /usr/local/bin/vault operator init -address=http://127.0.0.1:8200 -key-shares=5 -key-threshold=3 > vault-init-keys.txt
+  export VAULT_ADDR=http://127.0.0.1:82${INSTANCE}0
+  echo "Initializing Vault and storing keys for $INSTANCE instance..."
+  sudo /usr/local/bin/vault operator init -address=http://127.0.0.1:82${INSTANCE}0 -key-shares=5 -key-threshold=3 > $CONFIG_DIR/vault-init-keys.txt
 
   if [ $? -ne 0 ]; then
-    echo "Error: Failed to inialize Vault."
+    echo "Error: Failed to initialize Vault."
     exit 1
   fi
 
-  echo "Vault initialized. Keys and root token are stored in vault-init-keys.txt"
-  echo "Initialization complete."
+  echo "Vault initialized. Keys and root token are stored in $CONFIG_DIR/vault-init-keys.txt"
+  echo "Initialization complete for $INSTANCE instance."
 
   echo "Unsealing vault..."
   bash vault-auto-unseal.sh
 
-  echo "Scheduling job to auto unseal ..."
-  autounsealcmd="bash vault-auto-unseal.sh > /opt/vault_cron_logs/auto_unseal.log 2>&1"
+  echo "Scheduling job to auto unseal..."
+  autounsealcmd="bash vault-auto-unseal.sh > /opt/vault_cron_logs/$INSTANCE/auto_unseal.log 2>&1"
   autounsealcronjob="*/2 * * * * $autounsealcmd"
   ( crontab -l | grep -v -F "$autounsealcmd" || : ; echo "$autounsealcronjob" ) | crontab -
 
-  echo "Scheduling cron for automated backups once in every 8 hours..."
-  croncmd="vault operator raft snapshot save $backup_path/vault_backup_`$(date +\%Y\%m\%d\%H\%M\%S)`.snap >> /opt/vault_cron_logs/vault_backup.log 2>&1"
+  echo "Scheduling cron for automated backups for $INSTANCE instance every 8 hours..."
+  croncmd="vault operator raft snapshot save $backup_path/$INSTANCE/vault_backup_\`date +\\%Y\\%m\\%d\\%H\\%M\\%S\`.snap >> /opt/vault_cron_logs/$INSTANCE/vault_backup.log 2>&1"
   cronjob="0 */8 * * * $croncmd"
   ( crontab -l | grep -v -F "$croncmd" || : ; echo "$cronjob" ) | crontab -
 
-  echo "Scheduling cron for cleaning up snapshots older than $RETENTION_DAYS ..."
-  cleanupcmd="find $backup_path -type f -name 'vault_backup_*.snap' -mtime +$RETENTION_DAYS -exec rm -f {} \; >> /opt/vault_cron_logs/vault_cleanup.log 2>&1"
+  echo "Scheduling cron for cleaning up snapshots older than $RETENTION_DAYS for $INSTANCE instance..."
+  cleanupcmd="find $backup_path/$INSTANCE -type f -name 'vault_backup_*.snap' -mtime +$RETENTION_DAYS -exec rm -f {} \; >> /opt/vault_cron_logs/$INSTANCE/vault_cleanup.log 2>&1"
   cleanupjob="* 4 * * * $cleanupcmd"
   ( crontab -l | grep -v -F "$cleanupcmd" || : ; echo "$cleanupjob" ) | crontab -
 }
 
-
 uninstall_vault() {
+  local INSTANCE=$1
+  local SERVICE_NAME="vault-${INSTANCE}.service"
+  local CONFIG_DIR="/etc/vault.d/${INSTANCE}"
+  local DATA_DIR="/opt/data/vault/${INSTANCE}"
+
   if ! command -v vault &> /dev/null; then
     echo "Error: Vault is not installed."
     exit 1
   fi
 
-  echo "Stopping and disabling Vault service..."
-  sudo systemctl stop vault.service
-  sudo systemctl disable vault.service
+  echo "Stopping and disabling Vault service for $INSTANCE instance..."
+  sudo systemctl stop $SERVICE_NAME
+  sudo systemctl disable $SERVICE_NAME
 
-  echo "Removing vault installation..."
+  echo "Removing vault installation for $INSTANCE instance..."
   sudo rm -f /usr/local/bin/vault
-  sudo rm -rf /opt/data/vault
-  sudo rm -rf /etc/vault.d
-  sudo rm -rf /etc/systemd/system/vault.service
-  sudo rm -rf LICENSE.txt
-  sudo rm -rf vault-init-keys.txt
+  sudo rm -rf $DATA_DIR
+  sudo rm -rf $CONFIG_DIR
+  sudo rm -f /etc/systemd/system/$SERVICE_NAME
+  sudo rm -rf $CONFIG_DIR/vault-init-keys.txt
 
-  echo "Removing vault user..."
-  sudo userdel -r vault 2>/dev/null
-  sudo systemctl daemon-reload
-  echo "Removing crontab entry"
+  echo "Removing crontab entries for $INSTANCE instance..."
   ( crontab -l | grep -v -F "$croncmd" ) | crontab -
-  ( crontab -l | grep -v -F "$cleanupjob" ) | crontab -
+  ( crontab -l | grep -v -F "$cleanupcmd" ) | crontab -
   ( crontab -l | grep -v -F "$autounsealcronjob" ) | crontab -
-  echo "Vault uninstalled successfully."
+
+  sudo systemctl daemon-reload
+  echo "Vault uninstalled successfully for $INSTANCE instance."
 }
 
-if [ $# -lt 1 ]; then
-  echo "Error: Command argument (install or uninstall) is required."
+if [ $# -lt 2 ]; then
+  echo "Error: Command arguments are required."
   usage
 fi
 
 COMMAND=$1
+INSTANCE=$2
 
 case $COMMAND in
   install)
-    if [ $# -ne 2 ]; then
+    if [ $# -ne 3 ]; then
       echo "Error: Vault version argument is required for installation."
       usage
     fi
-    VAULT_VERSION=$2
-    install_vault $VAULT_VERSION
+    VAULT_VERSION=$3
+    install_vault $INSTANCE $VAULT_VERSION
     ;;
   uninstall)
-    uninstall_vault
+    uninstall_vault $INSTANCE
     ;;
   *)
     echo "Error: Invalid command. Use either 'install' or 'uninstall'."

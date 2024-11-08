a28f9816775b2a85f42dc328de25d3a710a15805
Chittibabu Boya <bchittibabu100@gmail.com>
2024-11-07 19:22:55 -0800

    Update vault-auto-unseal.sh
    updated

--- vault-auto-unseal.sh
+++ vault-auto-unseal.sh
@@ -1,67 +1,39 @@
-#!/bin/bash
+import sys
+import os
+sys.path.append('/home/bogner/prs_eob_maker/')
+from mcds_eob_maker import create_builder
 
-# Define the instances and their corresponding addresses and key files
-declare -A instance_addresses=(
-  ["dev"]="http://127.0.0.1:8200"
-  ["stage"]="http://127.0.0.1:8300"
-  ["prod"]="http://127.0.0.1:8400"
-)
-declare -A key_files=(
-  ["dev"]="vault-init-keys-dev.txt"
-  ["stage"]="vault-init-keys-stage.txt"
-  ["prod"]="vault-init-keys-prod.txt"
-)
+CONFIG_FILE = '/home/bogner/hscsn/config.ini'
 
-# Check if instance name was provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <instance_name>"
-  echo "Available instances: ${!instance_addresses[@]}"
-  exit 1
-fi
 
-# Get the instance name from input parameter
-instance=$1
+def build_plain_eob(one_xml, auxFiles=None, fname="test", save=False):
+    
+    # Document System will pass in Pay Date as "Overlay Date" when calling eob_maker_runner.
+    # When ran locally - it will be None - and `create_builder` will set it to the current date.
+    payment_date = globals().get('overlay_date', None)
 
-# Validate instance name
-if [ -z "${instance_addresses[$instance]}" ]; then
-  echo "Error: Invalid instance name '$instance'."
-  echo "Available instances: ${!instance_addresses[@]}"
-  exit 1
-fi
+    builder = create_builder(CONFIG_FILE, payment_date=payment_date)
 
-# Set Vault address and key file based on instance
-export VAULT_ADDR="${instance_addresses[$instance]}"
-key_file="${key_files[$instance]}"
+    return builder(one_xml, auxFiles=auxFiles, fname=fname, save=save)
 
-echo "Checking Vault instance: $instance at $VAULT_ADDR"
-
-# Check if the instance is reachable
-if ! curl -s --connect-timeout 5 "${VAULT_ADDR}/v1/sys/health" &>/dev/null; then
-  echo "Warning: $instance Vault instance at $VAULT_ADDR is not reachable. Exiting."
-  exit 1
-fi
-
-# Check if Vault is sealed
-if /usr/local/bin/vault status -format json | jq ".sealed" | grep -q "false"; then
-  echo "$instance Vault is not sealed"
-else
-  echo "$instance Vault is sealed, unsealing now"
-  
-  # Check if key file exists
-  if [ ! -f "$key_file" ]; then
-    echo "Error: Unseal key file $key_file not found for $instance. Exiting."
-    exit 1
-  fi
-
-  # Read the unseal keys from the respective key file
-  key1=$(awk '/Unseal Key 1/{print $NF}' "$key_file")
-  key2=$(awk '/Unseal Key 2/{print $NF}' "$key_file")
-  key3=$(awk '/Unseal Key 3/{print $NF}' "$key_file")
-  
-  # Unseal the Vault instance with each key
-  /usr/local/bin/vault operator unseal "$key1"
-  /usr/local/bin/vault operator unseal "$key2"
-  /usr/local/bin/vault operator unseal "$key3"
-  
-  echo "$instance Vault unsealed."
-fi
+if __name__ == "__main__":
+    from glob import glob
+    if len(sys.argv) > 1:
+        # When in the debugger, we need globbing to get wildcards.
+        # When in the shell, the shell expands the wildcards into
+        # multiple arguments.  Doing this loop will cover all cases.
+        for globTarg in sys.argv[1:]:
+            files = glob(os.path.expandvars(os.path.expanduser(globTarg)))
+            for path in files:
+                with open(path, 'rb') as xml_handle:
+                    xml = xml_handle.read()
+                    fName = os.path.splitext(os.path.basename(path))[0]
+                    pdfPath = os.path.dirname(path)
+                    pdffile = os.path.join(pdfPath, fName) + ".pdf"
+                    raw = build_plain_eob(xml, fname=pdffile, save=False)
+                    with open(pdffile, "wb") as f:
+                        f.write(raw)
+                    print("Generated %s" % pdffile)
+    else:
+        print("No file argument")
+    print("Finished")

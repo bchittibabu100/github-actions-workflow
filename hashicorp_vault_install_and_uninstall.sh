f83b9be36974a3b2f4fb35fc4986c783a99ddd6e
Chittibabu Boya <bchittibabu100@gmail.com>
2024-11-29 22:08:26 -0800

    Update hashicorp_vault_install_and_uninstall.sh
    test

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,25 +1,32 @@
-Here is the docker file contents and Jfrog Xray CVE's report. give me updated docker file to get rid off all these CVE's
-
+# Build arguments
 ARG REGISTRYURL=docker.repo1.vhc.com/vpay-docker
 ARG DOTNET_SDK_VERSION=8.0.403
 ARG DOTNET_RUNTIME_VERSION=8.0.4
-
 ARG DOTNET_SDK_VARIANT=jammy
 ARG DOTNET_RUNTIME_VARIANT=jammy-db2
 ARG BASE_SDK_IMAGE=dotnet/sdk
 ARG BASE_RUNTIME_IMAGE=dotnet/aspnet
 
+# Use the base SDK image with the latest updates
 FROM ${REGISTRYURL}/base-images/${BASE_SDK_IMAGE}:${DOTNET_SDK_VERSION}-${DOTNET_SDK_VARIANT} as base
 WORKDIR /app
 
-FROM base as build
+# Update base image and install security patches
+RUN apt-get update && \
+    apt-get upgrade -y && \
+    apt-get install -y --no-install-recommends \
+    # Security updates for vulnerable libraries
+    openssl libkrb5-3 libkrb5support0 libssl3 libgssapi-krb5-2 libk5crypto3 \
+    libc-bin libc6 libpcre3 libzstd1 libpcre2-8-0 && \
+    apt-get clean
 
+# Build stage
+FROM base as build
 ARG CONFIG_PROFILE=Release
 ARG PROJECT_DIR
 ARG PROJECT_NAME
 
 ENV PROJECT=${PROJECT_DIR}/${PROJECT_NAME}.csproj
-
 
 COPY nuget.config* ./
 COPY *.sln ./
@@ -35,13 +42,23 @@ RUN dotnet restore ${PROJECT}
 COPY . ./
 RUN dotnet publish --no-restore -c ${CONFIG_PROFILE} -o /app/out ${PROJECT}
 
+# Runtime image with the necessary security updates
 FROM ${REGISTRYURL}/base-images/${BASE_RUNTIME_IMAGE}:${DOTNET_RUNTIME_VERSION}-${DOTNET_RUNTIME_VARIANT} AS final
-ARG PROJECT_NAME
 
+# Update runtime image and install security patches
+RUN apt-get update && \
+    apt-get upgrade -y && \
+    apt-get install -y --no-install-recommends \
+    openssl libkrb5-3 libkrb5support0 libssl3 libgssapi-krb5-2 libk5crypto3 \
+    libc-bin libc6 libpcre3 libzstd1 libpcre2-8-0 && \
+    apt-get clean
+
+# Set timezone and configure the application
 RUN ln -fs /usr/share/zoneinfo/America/Chicago /etc/localtime && dpkg-reconfigure -f noninteractive tzdata
 
 WORKDIR /app
 
+# Copy the published output from the build stage
 COPY --from=build /app/out .
 ENV ASPNETCORE_URLS=http://+:80
 
@@ -51,24 +68,3 @@ ENTRYPOINT [ "dotnet", "Entrypoint.dll" ]
 
 ARG IMAGE_BUILD_TIME
 ENV IMAGE_BUILD_TIME ${IMAGE_BUILD_TIME}
-
-
-CVE-2024-37371_ubuntu:jammy:libgssapi-krb5-2_1.19.2-2ubuntu0.3
-CVE-2024-37371_ubuntu:jammy:libk5crypto3_1.19.2-2ubuntu0.3
-CVE-2022-40735_ubuntu:jammy:openssl_3.0.2-0ubuntu1.15
-CVE-2024-37371_ubuntu:jammy:libkrb5support0_1.19.2-2ubuntu0.3
-CVE-2022-40735_ubuntu:jammy:libssl3_3.0.2-0ubuntu1.15
-CVE-2024-37371_ubuntu:jammy:libkrb5-3_1.19.2-2ubuntu0.3
-CVE-2024-37370_ubuntu:jammy:libk5crypto3_1.19.2-2ubuntu0.3
-CVE-2024-37370_ubuntu:jammy:libgssapi-krb5-2_1.19.2-2ubuntu0.3
-CVE-2024-37370_ubuntu:jammy:libkrb5support0_1.19.2-2ubuntu0.3
-CVE-2024-37370_ubuntu:jammy:libkrb5-3_1.19.2-2ubuntu0.3
-CVE-2018-5709_ubuntu:jammy:libkrb5-3_1.19.2-2ubuntu0.3
-CVE-2018-5709_ubuntu:jammy:libkrb5support0_1.19.2-2ubuntu0.3
-CVE-2018-5709_ubuntu:jammy:libgssapi-krb5-2_1.19.2-2ubuntu0.3
-CVE-2017-11164_ubuntu:jammy:libpcre3:2_8.39-13ubuntu0.22.04.1
-CVE-2016-20013_ubuntu:jammy:libc-bin_2.35-0ubuntu3.7
-CVE-2022-4899_ubuntu:jammy:libzstd1_1.4.8+dfsg-3build1
-CVE-2016-20013_ubuntu:jammy:libc6_2.35-0ubuntu3.7
-CVE-2022-41409_ubuntu:jammy:libpcre2-8-0_10.39-3ubuntu0.1
-CVE-2018-5709_ubuntu:jammy:libk5crypto3_1.19.2-2ubuntu0.3

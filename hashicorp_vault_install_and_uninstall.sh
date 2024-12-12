cd9520d22ed9b69c13e3f6692107049410550495
Chittibabu Boya <bchittibabu100@gmail.com>
2024-12-12 09:14:30 -0800

    Update hashicorp_vault_install_and_uninstall.sh
    test

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,5 +1,1 @@
-            this.GitRevision = System.Environment.GetEnvironmentVariable("RELEASE_COMMIT_SHA") ?? "[[GitRevision]]";
-            this.BuildTime = System.Environment.GetEnvironmentVariable("IMAGE_BUILD_TIME") ?? "[[BuildTime]]";
-            this.Environment = System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            this.VersionInfo = System.Environment.GetEnvironmentVariable("RELEASE_TAG") ?? "[[VersionInfo]]";
-            this.DataCenter = System.Environment.GetEnvironmentVariable("DEPLOYMENT_DATACENTER") ?? "[[DataCenter]]";
+sudo systemctl restart -H bamboosa@asstglds01.express.net reload-or-restart kestrel-vpay-docsys-parsing-api.service

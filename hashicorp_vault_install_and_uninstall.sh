b1683a6d0dce51b0cb16fbda943897d02257ebc7
Chittibabu Boya <bchittibabu100@gmail.com>
2024-12-14 12:56:20 -0800

    Update hashicorp_vault_install_and_uninstall.sh
    test

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,1 +1,14 @@
-sudo systemctl restart -H bamboosa@asstglds01.express.net reload-or-restart kestrel-vpay-docsys-parsing-api.service
+      - name: Copy artifacts
+        run: |
+          scp artifacts/* bamboosa@asstglds01.example.net:/usr/local/example/docsys/parsing
+          scp artifacts/* bamboosa@asstglds02.example.net:/usr/local/example/docsys/parsing
+          scp artifacts/* bamboosa@asstglds03.example.net:/usr/local/example/docsys/parsing
+        shell: bash
+
+      - name: Restart Parsing API service
+        if: env.env_name == 'stage'
+        run: |
+          ssh bamboosa@asstglds01.example.net "sudo systemctl reload-or-restart kestrel-example-docsys-parsing-api.service"
+          ssh bamboosa@asstglds02.example.net "sudo systemctl reload-or-restart kestrel-example-docsys-parsing-api.service"
+          ssh bamboosa@asstglds03.example.net "sudo systemctl reload-or-restart kestrel-example-docsys-parsing-api.service"
+        shell: bash

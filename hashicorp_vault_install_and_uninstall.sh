22edf7b954529e3cb78d5eb99b591320e3f4d997
Chittibabu Boya <bchittibabu100@gmail.com>
2024-12-17 10:16:04 -0800

    Update hashicorp_vault_install_and_uninstall.sh
    test

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,6 +1,13 @@
 $files = Get-ChildItem -Path $path -Recurse -Filter "*.Tests.csproj" | select Name, DirectoryName, BaseName, FullName
+
 ForEach ($file in $files)
 {
-Write-Host $file.Name
-dotnet test  $file.FullName  --logger "trx;LogFileName=${bamboo.build.working.directory}\TestResults\$($file.Name).trx" --filter "Category!=Integration"
+  Write-Host $file.Name
+  dotnet test  $file.FullName  --logger "trx;LogFileName=${bamboo.build.working.directory}\TestResults\$($file.Name).trx" --filter "Category!=Integration"
+
+  # return failure code
+  if ($LastExitCode -ne 0) {
+      $error | Write-Error $_
+      exit $LastExitCode
+  }
 }

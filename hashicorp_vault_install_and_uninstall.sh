1e7815b43f4bbf20f556f34874e9ce99409c7dc8
Chittibabu Boya <bchittibabu100@gmail.com>
2024-12-18 22:09:37 -0800

    Update hashicorp_vault_install_and_uninstall.sh
    test

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,56 +1,36 @@
-Here is the contents of the csproj file.
-
-<Project Sdk="Microsoft.NET.Sdk.Web">
-
-  <PropertyGroup>
-    <TargetFramework>net6.0</TargetFramework>
-    <DockerComposeProjectPath>..\..\docker-compose.dcproj</DockerComposeProjectPath>
-
-    <RuntimeIdentifiers>win10-x64;win10-x86;centos.7-x64;</RuntimeIdentifiers>
-    <SuppressDockerTargets>True</SuppressDockerTargets>
-
-    <GenerateAssemblyConfigurationAttribute>false</GenerateAssemblyConfigurationAttribute>
-    <GenerateAssemblyCompanyAttribute>false</GenerateAssemblyCompanyAttribute>
-    <GenerateAssemblyProductAttribute>false</GenerateAssemblyProductAttribute>
-  </PropertyGroup>
-
-  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|AnyCPU'">
-    <DocumentationFile>bin\Debug\net6.0\VPay.DocSys.Parsing.Api.xml</DocumentationFile>
-    <NoWarn>1701;1702;1705;1591</NoWarn>
-  </PropertyGroup>
-
-  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|AnyCPU'">
-    <DocumentationFile>bin\Release\net6.0\VPay.DocSys.Parsing.Api.xml</DocumentationFile>
-    <NoWarn>1701;1702;1705;1591;NU1701</NoWarn>
-  </PropertyGroup>
-
-  <ItemGroup>
-    <PackageReference Include="FluentValidation.AspNetCore" Version="11.2.2" />
-    <PackageReference Include="GlobalExceptionHandler" Version="4.0.2" />
-    <PackageReference Include="Microsoft.AspNetCore.Mvc.Versioning" Version="5.0.0" />
-    <PackageReference Include="Microsoft.AspNetCore.Mvc.NewtonsoftJson" Version="6.0.13" />
-    <PackageReference Include="Microsoft.Extensions.Hosting" Version="7.0.0" />
-    <PackageReference Include="Microsoft.Extensions.Configuration" Version="7.0.0" />
-    <PackageReference Include="Microsoft.Extensions.Configuration.Abstractions" Version="7.0.0" />
-    <PackageReference Include="Microsoft.Extensions.Configuration.Binder" Version="7.0.0" />
-    <PackageReference Include="Swashbuckle.AspNetCore" Version="6.4.0" />
-    <PackageReference Include="VPay.DocSys.HealthChecks" Version="2.1.9" />
-    <PackageReference Include="VPay.DocSys.HealthChecks.AspNetcore" Version="2.1.9" />
-    <PackageReference Include="VPay.DocSys.HealthChecks.FileSystem" Version="2.1.9" />
-    <PackageReference Include="VPay.DocSys.HealthChecks.HttpEndpoint" Version="2.1.9" />
-    <PackageReference Include="VPay.DocSys.HealthChecks.Network" Version="2.1.9" />
-    <PackageReference Include="VPay.DocSys.HealthChecks.Odbc" Version="2.1.9" />
-    <PackageReference Include="VPay.DocSys.HealthChecks.RabbitMQ" Version="2.1.9" />
-    <PackageReference Include="VPay.DocSys.HealthChecks.SqlServer" Version="2.1.9" />
-    <PackageReference Include="VPay.DocSys.HealthChecks.StorageManager" Version="2.1.9" />
-    <PackageReference Include="VPay.Queuing.DependencyInjection" Version="1.3.1" />
-    <PackageReference Include="VPay.Extensions.Logging.GrayLog" Version="0.6.7" />
-  </ItemGroup>
-  <ItemGroup>
-    <ProjectReference Include="..\VPay.DocSys.Parsing\VPay.DocSys.Parsing.csproj" />
-  </ItemGroup>
-</Project>
-
-
-Here is the dotnet publish command
-dotnet publish ./src/VPay.DocSys.Parsing.Api/VPay.DocSys.Parsing.Api.csproj -c Release -r centos.7-x64 -o ./artifacts
+  Determining projects to restore...
+  Restored /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Http/VPay.DocSys.Parsing.Http.csproj (in 533 ms).
+  Restored /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Common/VPay.DocSys.Parsing.Common.csproj (in 602 ms).
+  Restored /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.Http.Tests/VPay.DocSys.Parsing.Http.Tests.csproj (in 618 ms).
+  Restored /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.Common.Tests/VPay.DocSys.Parsing.Common.Tests.csproj (in 619 ms).
+  Restored /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Inbound835FileQueueProcessor/VPay.DocSys.Parsing.Inbound835FileQueueProcessor.csproj (in 645 ms).
+  Restored /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.InventoryDriverQueueProcessor/VPay.DocSys.Parsing.InventoryDriverQueueProcessor.csproj (in 655 ms).
+  Restored /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing/VPay.DocSys.Parsing.csproj (in 656 ms).
+  Restored /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.QueueProcessor.Consolidated/VPay.DocSys.Parsing.QueueProcessor.Consolidated.csproj (in 657 ms).
+  Restored /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.OutboundRedCardDataQueueProcessor/VPay.DocSys.Parsing.OutboundRedCardDataQueueProcessor.csproj (in 657 ms).
+  Restored /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.OutboundRedCardDataQueueProcessor.Tests/VPay.DocSys.Parsing.OutboundRedCardDataQueueProcessor.Tests.csproj (in 679 ms).
+  Restored /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.InventoryDriverQueueProcessor.Tests/VPay.DocSys.Parsing.InventoryDriverQueueProcessor.Tests.csproj (in 682 ms).
+  Restored /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.QueueProcessor.Consolidated.Tests/VPay.DocSys.Parsing.QueueProcessor.Consolidated.Tests.csproj (in 651 ms).
+  Restored /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.Inbound835FileQueueProcessor.Tests/VPay.DocSys.Parsing.Inbound835FileQueueProcessor.Tests.csproj (in 681 ms).
+  Restored /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.Tests/VPay.DocSys.Parsing.Tests.csproj (in 682 ms).
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.Api.Tests/VPay.DocSys.Parsing.Api.Tests.csproj : error NU1605: Detected package downgrade: Microsoft.Extensions.Hosting from 7.0.0 to 6.0.0. Reference the package directly from the project to select a different version.  [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.Api.Tests/VPay.DocSys.Parsing.Api.Tests.csproj : error NU1605:  VPay.DocSys.Parsing.Api.Tests -> VPay.DocSys.Parsing.Api -> VPay.DocSys.Parsing -> VPay.DocSys.Parsing.Http -> Microsoft.Extensions.Hosting (>= 7.0.0)  [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.Api.Tests/VPay.DocSys.Parsing.Api.Tests.csproj : error NU1605:  VPay.DocSys.Parsing.Api.Tests -> VPay.DocSys.Parsing.Api -> Microsoft.Extensions.Hosting (>= 6.0.0) [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.Api.Tests/VPay.DocSys.Parsing.Api.Tests.csproj : error NU1605: Detected package downgrade: Microsoft.Extensions.Configuration.Abstractions from 7.0.0 to 6.0.0. Reference the package directly from the project to select a different version.  [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.Api.Tests/VPay.DocSys.Parsing.Api.Tests.csproj : error NU1605:  VPay.DocSys.Parsing.Api.Tests -> VPay.DocSys.Parsing.Api -> VPay.DocSys.Parsing -> VPay.DocSys.Parsing.Common -> Microsoft.Extensions.Options.ConfigurationExtensions 7.0.0 -> Microsoft.Extensions.Configuration.Abstractions (>= 7.0.0)  [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.Api.Tests/VPay.DocSys.Parsing.Api.Tests.csproj : error NU1605:  VPay.DocSys.Parsing.Api.Tests -> VPay.DocSys.Parsing.Api -> Microsoft.Extensions.Configuration.Abstractions (>= 6.0.0) [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.Api.Tests/VPay.DocSys.Parsing.Api.Tests.csproj : error NU1605: Detected package downgrade: Microsoft.Extensions.Configuration.Binder from 7.0.0 to 6.0.0. Reference the package directly from the project to select a different version.  [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.Api.Tests/VPay.DocSys.Parsing.Api.Tests.csproj : error NU1605:  VPay.DocSys.Parsing.Api.Tests -> VPay.DocSys.Parsing.Api -> VPay.DocSys.Parsing -> VPay.DocSys.Parsing.Common -> Microsoft.Extensions.Options.ConfigurationExtensions 7.0.0 -> Microsoft.Extensions.Configuration.Binder (>= 7.0.0)  [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.Api.Tests/VPay.DocSys.Parsing.Api.Tests.csproj : error NU1605:  VPay.DocSys.Parsing.Api.Tests -> VPay.DocSys.Parsing.Api -> Microsoft.Extensions.Configuration.Binder (>= 6.0.0) [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+  Failed to restore /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/test/VPay.DocSys.Parsing.Api.Tests/VPay.DocSys.Parsing.Api.Tests.csproj (in 1.32 sec).
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Api/VPay.DocSys.Parsing.Api.csproj : error NU1605: Detected package downgrade: Microsoft.Extensions.Hosting from 7.0.0 to 6.0.0. Reference the package directly from the project to select a different version.  [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Api/VPay.DocSys.Parsing.Api.csproj : error NU1605:  VPay.DocSys.Parsing.Api -> VPay.DocSys.Parsing -> VPay.DocSys.Parsing.Http -> Microsoft.Extensions.Hosting (>= 7.0.0)  [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Api/VPay.DocSys.Parsing.Api.csproj : error NU1605:  VPay.DocSys.Parsing.Api -> Microsoft.Extensions.Hosting (>= 6.0.0) [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Api/VPay.DocSys.Parsing.Api.csproj : error NU1605: Detected package downgrade: Microsoft.Extensions.Configuration.Abstractions from 7.0.0 to 6.0.0. Reference the package directly from the project to select a different version.  [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Api/VPay.DocSys.Parsing.Api.csproj : error NU1605:  VPay.DocSys.Parsing.Api -> VPay.DocSys.Parsing -> VPay.DocSys.Parsing.Common -> Microsoft.Extensions.Options.ConfigurationExtensions 7.0.0 -> Microsoft.Extensions.Configuration.Abstractions (>= 7.0.0)  [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Api/VPay.DocSys.Parsing.Api.csproj : error NU1605:  VPay.DocSys.Parsing.Api -> Microsoft.Extensions.Configuration.Abstractions (>= 6.0.0) [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Api/VPay.DocSys.Parsing.Api.csproj : error NU1605: Detected package downgrade: Microsoft.Extensions.Configuration.Binder from 7.0.0 to 6.0.0. Reference the package directly from the project to select a different version.  [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Api/VPay.DocSys.Parsing.Api.csproj : error NU1605:  VPay.DocSys.Parsing.Api -> VPay.DocSys.Parsing -> VPay.DocSys.Parsing.Common -> Microsoft.Extensions.Options.ConfigurationExtensions 7.0.0 -> Microsoft.Extensions.Configuration.Binder (>= 7.0.0)  [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Api/VPay.DocSys.Parsing.Api.csproj : error NU1605:  VPay.DocSys.Parsing.Api -> Microsoft.Extensions.Configuration.Binder (>= 6.0.0) [/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/VPay.DocSys.Parsing.sln]
+  Failed to restore /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Api/VPay.DocSys.Parsing.Api.csproj (in 1.72 sec).
+  1 of 17 projects are up-to-date for restore.

19a2161ffaa61527893daf715c4ce9e5df1149df
Chittibabu Boya <bchittibabu100@gmail.com>
2025-02-08 20:57:35 -0800

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,6 +1,6 @@
-gitrunner@mo066inflrun05 ~/actions-runner/_work/vpay-abc/vpay-abc/vpay-abc $ansible-playbook /home/gitrunner/playbook_deploy.yaml -i /home/gitrunner/inventory.ini -e "reponame=vpay-abc" -e "dest_dir=/home/bogner/vpay-abc" -e "extra_excludes=" -C -u bogner
+gitrunner@mo066inflrun05 ~/actions-runner/_work/vpay-abc/vpay-abc/vpay-abc $ansible-playbook /home/gitrunner/playbook_deploy.yaml -i /home/gitrunner/inventory.ini -e "reponame=tpa-abc" -e "dest_dir=/home/bogner/tpa-abc" -e "extra_excludes=" -C -u bogner
 
 PLAY [Deploy Application] *************************************************************************************************************************************************
 
 TASK [Gathering Facts] ****************************************************************************************************************************************************
-fatal: [asstglds01.vpayusa.net]: FAILED! => {"ansible_facts": {}, "changed": false, "failed_modules": {"ansible.legacy.setup": {"failed": true, "module_stderr": "Shared connection to asstglds01.vpayusa.net closed.\r\n", "module_stdout": "/bin/sh: /root/.pyenv/shims/python: No such file or directory\r\n", "msg": "MODULE FAILURE\nSee stdout/stderr for the exact error", "rc": 127}}, "msg": "The following modules failed to execute: ansible.legacy.setup\n"}
+fatal: [asstglds01.tpa.net]: FAILED! => {"ansible_facts": {}, "changed": false, "failed_modules": {"ansible.legacy.setup": {"failed": true, "module_stderr": "Shared connection to asstglds01.tpa.net closed.\r\n", "module_stdout": "/bin/sh: /root/.pyenv/shims/python: No such file or directory\r\n", "msg": "MODULE FAILURE\nSee stdout/stderr for the exact error", "rc": 127}}, "msg": "The following modules failed to execute: ansible.legacy.setup\n"}

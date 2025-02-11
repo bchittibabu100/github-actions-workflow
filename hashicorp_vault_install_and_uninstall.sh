a1de2f445d0c57825b3babb42319737208353d34
Chittibabu Boya <bchittibabu100@gmail.com>
2025-02-11 14:04:48 -0800

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,47 +1,30 @@
----
-- name: Deploy Application
-  hosts: all
-  become: yes
-  become_user: bogner
-  vars:
-    ansible_python_interpreter: /home/bogner/.pyenv/shims/python  # Ensure this is set to the correct Python interpreter on the remote server
-  tasks:
-    - name: Check if dest_dir exists and is valid on the remote server
-      stat:
-        path: "{{ dest_dir }}"
-      register: dest_dir_status
-
-    - name: Fail if dest_dir is invalid
-      fail:
-        msg: "The destination directory {{ dest_dir }} is not valid. It must be within /home/bogner."
-      when: dest_dir_status.stat.exists == False or not dest_dir | regex_search('^/home/bogner/')
-
-    - name: Archive old files if dest_dir exists on the remote server
-      shell: |
-        TIMESTAMP=$(date +%H_%M_%S)
-        ARCHIVE_DIR="/SRVFS/bogner/deploymentBackup/{{ inventory_hostname }}/{{ reponame }}/$(date +%Y/%m/%d)/$TIMESTAMP/"
-        
-        # Archive existing files if the directory exists
-        if [ -d "{{ dest_dir }}" ]; then
-          mkdir -p "$ARCHIVE_DIR"
-          cd "{{ dest_dir }}"
-          tar --remove-files --exclude "*.egg-info" -cf "$ARCHIVE_DIR/$TIMESTAMP.tgz" "{{ dest_dir }}/"*
-        fi
-      register: archive_result
-      when: dest_dir_status.stat.exists == True
-
-    - name: Deploy to the remote server
-      copy:
-        src: "{{ reponame }}/"
-        dest: "{{ dest_dir }}"
-        remote_src: yes
-      when: archive_result.changed == False  # If archive did not happen, proceed with deployment
-
-    - name: Restore from archive if deploy fails
-      shell: |
-        cd "$ARCHIVE_DIR"
-        tar -xvf "$TIMESTAMP.tgz"
-        rm "$TIMESTAMP.tgz"
-        mv "$ARCHIVE_DIR"/* "{{ dest_dir }}/"
-      when: archive_result.changed == True
-      failed_when: archive_result.failed
+2025-02-11T21:57:40.3062079Z Requested runner group: tpay-runner-group
+2025-02-11T21:57:40.3062248Z Job defined at: tpay-financial/tpay-bamboo_deployment/.github/workflows/manual-deploy.yml@refs/heads/github
+2025-02-11T21:57:40.3062452Z Reusable workflow chain:
+2025-02-11T21:57:40.3062540Z tpay-financial/tpay-vpa1/.github/workflows/manual-deploy.yml@refs/heads/PAY-206946_IM (7b9ba4a5345c234489cf445e5bdfac59ea3aa46c)
+2025-02-11T21:57:40.3062612Z -> tpay-financial/tpay-bamboo_deployment/.github/workflows/manual-deploy.yml@refs/heads/github (52fbf8b2c5555bd7f7e591cca989e76df07b5eb9)
+2025-02-11T21:57:40.3062709Z Waiting for a runner to pick up this job...
+2025-02-11T21:57:40.4720932Z Job is about to start running on the runner: tpay-mo066inflrun01 (organization)
+2025-02-11T21:57:43.4828282Z Current runner version: '2.322.0'
+2025-02-11T21:57:43.4837932Z Runner name: 'tpay-mo066inflrun01'
+2025-02-11T21:57:43.4839419Z Runner group name: 'tpay-runner-group'
+2025-02-11T21:57:43.4840801Z Machine name: 'mo066inflrun01'
+2025-02-11T21:57:43.4846147Z ##[group]GITHUB_TOKEN Permissions
+2025-02-11T21:57:43.4849789Z Actions: write
+2025-02-11T21:57:43.4850514Z Attestations: write
+2025-02-11T21:57:43.4851159Z Checks: write
+2025-02-11T21:57:43.4851813Z Contents: write
+2025-02-11T21:57:43.4852471Z Deployments: write
+2025-02-11T21:57:43.4853213Z Discussions: write
+2025-02-11T21:57:43.4853894Z Issues: write
+2025-02-11T21:57:43.4854531Z Metadata: read
+2025-02-11T21:57:43.4855161Z Packages: write
+2025-02-11T21:57:43.4855886Z Pages: write
+2025-02-11T21:57:43.4856554Z PullRequests: write
+2025-02-11T21:57:43.4857258Z RepositoryProjects: write
+2025-02-11T21:57:43.4858000Z SecurityEvents: write
+2025-02-11T21:57:43.4858669Z Statuses: write
+2025-02-11T21:57:43.4859338Z ##[endgroup]
+2025-02-11T21:57:43.4862609Z Secret source: Actions
+2025-02-11T21:57:43.4863887Z Prepare workflow directory
+2025-02-11T21:57:43.5603526Z ##[error]One or more errors occurred. (Access to the path '/home/gitrunner/actions-runner/_work/_actions/actions/checkout/v4.completed' is denied.) (Access to the path '/home/gitrunner/actions-runner/_work/_actions/actions/checkout/v4/.gitignore' is denied.) (Access to the path '/home/gitrunner/actions-runner/_work/_actions/actions/checkout/v4/.prettierignore' is denied.)

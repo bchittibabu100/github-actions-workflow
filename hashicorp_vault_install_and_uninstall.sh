fcb965a8094e2a8fd5cf970a5b1fa8e9d9022943
Chittibabu Boya <bchittibabu100@gmail.com>
2025-02-13 11:06:08 -0800

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,6 +1,27 @@
-Run actions/upload-artifact@v4
-With the provided path, there will be 138 files uploaded
-Artifact name is valid!
-Root directory input is valid!
-Beginning upload of artifact content to blob storage
-Error: request to https://productionresultssa3.blob.core.windows.net/actions-results/e69fa346-0675-4815-bb3b-071e9fd7abac/workflow-job-run-ca395085-040a-526b-2ce8-bdc85f692774/artifacts/96406c32d83f18d1aac06a3501fa43ef84886e19f4386812b3ef85a77aaf41b4.zip?se=2025-02-12T04%3A00%3A27Z&sig=uw50PTIWoz3ON%2FTS3X%2BzvoBKAe5wnpW%2FAHeF%2FFGgUR4%3D&ske=2025-02-12T11%3A37%3A37Z&skoid=ca7593d4-ee42-46cd-af88-8b886a2f84eb&sks=b&skt=2025-02-11T23%3A37%3A37Z&sktid=398a6654-997b-47e9-b12b-9515b896b4de&skv=2025-01-05&sp=cw&spr=https&sr=b&st=2025-02-12T03%3A00%3A22Z&sv=2025-01-05&comp=block&blockid=NjlmNWYxY2MtOTEwNy00NDAwLWEyNWEtMzZhOGQ2MzM0YTIwMDAwMDAwMDAwMDAw failed, reason: self-signed certificate in certificate chain
+      - name: Generate checksum for artifacts
+        run: |
+          tar -czvf saved_artifacts.tgz artifacts
+          original_checksum=`md5sum saved_artifacts.tgz`
+          echo "original_checksum=$original_checksum | cut -d' ' -f 1"  >> $GITHUB_ENV
+          echo $original_checksum
+        shell: bash
+
+  deploy-stage:
+    if: ${{ github.event.inputs.env == 'stage' }}
+    needs: build
+    runs-on:
+      group: vpay-runner-group
+    steps:
+      - name: Download build artifacts
+        run: |
+          echo Original_Checksum: ${{ env.original_checksum }}
+          current_checksum=`md5sum saved_artifacts.tgz | cut -d' ' -f 1`
+          echo Current_Checksum: $current_checksum
+          if [ "${{ env.original_checksum }}" == "$current_checksum" ]; then
+            echo "Proceeding with the creation of directory"
+            mkdir saved_atifacts
+            tar -xzvf saved_artifacts.tgz --directory=saved_artifacts
+          else
+            echo "Make sure there are no other builds are in progress."
+          fi
+        shell: bash

a921dbf41f55d3f264015e819c628b7777f153b2
Chittibabu Boya <bchittibabu100@gmail.com>
2025-02-14 18:56:09 -0800

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,14 +1,41 @@
-export DEPLOYMENT_DATETIME="${{ github.event.inputs.deployment_date }} ${{ github.event.inputs.deployment_time }}"
-export DEPLOYMENT_TIMESTAMP=$(TZ="America/Chicago" date -d "$DEPLOYMENT_DATETIME" +"%s")
-export CURRENT_TIMESTAMP=$(date -u +"%s")
+gitrunner@mo066inflrun05 ~ $ansible-playbook -i inventory.ini playbook_copy_nfs.yaml -e repo_name=test-abc -e extra_excludes=123.txt,abc.yaml
 
-WAIT_TIME=$((DEPLOYMENT_TIMESTAMP - CURRENT_TIMESTAMP))
+PLAY [Copy Application to NFS] ********************************************************************************************************************************************
 
-if [[ "$WAIT_TIME" -le 0 ]]; then
-  echo "Scheduled time is in the past! Exiting."
-  exit 1
-fi
+TASK [Merge exclude_files and extra_excludes] *****************************************************************************************************************************
+fatal: [asstglds03.test.net]: FAILED! => {"msg": "Unexpected templating type error occurred on ({{ exclude_files + extra_excludes }}): can only concatenate list (not \"str\") to list"}
 
-echo "Deployment is scheduled at: $(date -d "@$DEPLOYMENT_TIMESTAMP" -u)"
-echo "Waiting for $WAIT_TIME seconds..."
-echo "wait_time=$WAIT_TIME" >> $GITHUB_ENV
+PLAY RECAP ****************************************************************************************************************************************************************
+asstglds03.vpayusa.net     : ok=0    changed=0    unreachable=0    failed=1    skipped=0    rescued=0    ignored=0
+
+gitrunner@mo066inflrun05 ~ $cat playbook_copy_nfs.yaml
+---
+- name: Copy Application to NFS
+  hosts: asstglds03.vpayusa.net
+  gather_facts: no
+  vars:
+    reponame: "{{ reponame }}"
+    target_dir: "{{ reponame.split('-')[1] | default('') }}"
+    dest_dir: "/SRVFS/tpa_configs/{{ target_dir }}"
+    extra_excludes: "{{ extra_excludes | default([]) }}"
+
+  vars_files:
+    - exclude_files.yaml
+
+  tasks:
+    - name: Merge exclude_files and extra_excludes
+      set_fact:
+        final_excludes: "{{ exclude_files + extra_excludes }}"
+
+    - name: Printing final_excludes
+      debug:
+        var: final_excludes
+
+gitrunner@mo066inflrun05 ~ $cat exclude_files.yaml
+---
+exclude_files:
+  - "835_docs_generator"
+  - "bcf_transfer"
+  - "brdautosignature"
+  - "brdautosignature_py3"
+  - "brduploadmonitor"

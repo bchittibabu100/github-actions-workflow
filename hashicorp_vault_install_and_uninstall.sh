5a64f213fcd9dbd1cfacc3e52ae16e066f34ed3a
Chittibabu Boya <bchittibabu100@gmail.com>
2025-02-14 19:02:57 -0800

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,13 +1,3 @@
-gitrunner@mo066inflrun05 ~ $ansible-playbook -i inventory.ini playbook_copy_nfs.yaml -e repo_name=test-abc -e extra_excludes=123.txt,abc.yaml
-
-PLAY [Copy Application to NFS] ********************************************************************************************************************************************
-
-TASK [Merge exclude_files and extra_excludes] *****************************************************************************************************************************
-fatal: [asstglds03.test.net]: FAILED! => {"msg": "Unexpected templating type error occurred on ({{ exclude_files + extra_excludes }}): can only concatenate list (not \"str\") to list"}
-
-PLAY RECAP ****************************************************************************************************************************************************************
-asstglds03.vpayusa.net     : ok=0    changed=0    unreachable=0    failed=1    skipped=0    rescued=0    ignored=0
-
 gitrunner@mo066inflrun05 ~ $cat playbook_copy_nfs.yaml
 ---
 - name: Copy Application to NFS
@@ -17,7 +7,7 @@ gitrunner@mo066inflrun05 ~ $cat playbook_copy_nfs.yaml
     reponame: "{{ reponame }}"
     target_dir: "{{ reponame.split('-')[1] | default('') }}"
     dest_dir: "/SRVFS/tpa_configs/{{ target_dir }}"
-    extra_excludes: "{{ extra_excludes | default([]) }}"
+    extra_excludes: "{{ extra_excludes | default('') | split(',') }}"
 
   vars_files:
     - exclude_files.yaml
@@ -31,6 +21,16 @@ gitrunner@mo066inflrun05 ~ $cat playbook_copy_nfs.yaml
       debug:
         var: final_excludes
 
+gitrunner@mo066inflrun05 ~ $ansible-playbook -i inventory.ini playbook_copy_nfs.yaml -e "repo_name=test-abc" -e "extra_excludes=123.txt,abc.yaml"
+
+PLAY [Copy Application to NFS] ********************************************************************************************************************************************
+
+TASK [Merge exclude_files and extra_excludes] *****************************************************************************************************************************
+fatal: [asstglds03.test.net]: FAILED! => {"msg": "Unexpected templating type error occurred on ({{ exclude_files + extra_excludes }}): can only concatenate list (not \"str\") to list"}
+
+PLAY RECAP ****************************************************************************************************************************************************************
+asstglds03.vpayusa.net     : ok=0    changed=0    unreachable=0    failed=1    skipped=0    rescued=0    ignored=0
+
 gitrunner@mo066inflrun05 ~ $cat exclude_files.yaml
 ---
 exclude_files:
@@ -39,3 +39,30 @@ exclude_files:
   - "brdautosignature"
   - "brdautosignature_py3"
   - "brduploadmonitor"
+  - "brduploadmonitor_py3"
+  - "client_provider_report"
+  - "document_system"
+  - "dual_spec_process"
+  - "dual_spec_process_py3"
+  - "fax_server"
+  - "fileinterrogator"
+  - "fileinterrogator_py3"
+  - "fixed_length_process"
+  - "fixed_length_process_py3"
+  - "fssi"
+  - "fssi_cas"
+  - "herring_parser"
+  - "heq"
+  - "heq_provider_report"
+  - "importnonfundedpayment"
+  - "importnonfundedpayment_py3"
+  - "meta_check_images"
+  - "monday_reports"
+  - "parser_configs"
+  - "pep_file_process"
+  - "pep_file_process_py3"
+  - "rc"
+  - "recon"
+  - "red_card"
+  - "tigerteam"
+  - "transcard"

65bcb4c5ed0ead34f0a54bf5667042905db29f9f
Chittibabu Boya <bchittibabu100@gmail.com>
2025-02-14 19:12:22 -0800

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -8,7 +8,33 @@ ok: [asstglds03.test.net] => {
 }
 
 TASK [Merge exclude_files and extra_excludes] *****************************************************************************************************************************
-fatal: [asstglds03.vpayusa.net]: FAILED! => {"msg": "Unexpected templating type error occurred on ({{ exclude_files + extra_excludes }}): can only concatenate list (not \"str\") to list"}
+fatal: [asstglds03.test.net]: FAILED! => {"msg": "Unexpected templating type error occurred on ({{ exclude_files + extra_excludes }}): can only concatenate list (not \"str\") to list"}
 
 PLAY RECAP ****************************************************************************************************************************************************************
 asstglds03.vpayusa.net     : ok=1    changed=0    unreachable=0    failed=1    skipped=0    rescued=0    ignored=0
+
+gitrunner@mo066inflrun05 ~ $cat playbook_copy_nfs.yaml
+---
+- name: Copy Application to NFS
+  hosts: asstglds03.vpayusa.net
+  gather_facts: no
+  vars:
+    reponame: "{{ reponame }}"
+    target_dir: "{{ reponame.split('-')[1] | default('') }}"
+    dest_dir: "/SRVFS/tpa_configs/{{ target_dir }}"
+    extra_excludes: "{{ extra_excludes | default('') | split(',') | select('string') | list }}"
+
+  vars_files:
+    - exclude_files.yaml
+
+  tasks:
+    - name: Ensure extra_excludes is treated as a list
+      debug:
+        msg: "extra_excludes: {{ extra_excludes }}"
+    - name: Merge exclude_files and extra_excludes
+      set_fact:
+        final_excludes: "{{ exclude_files + extra_excludes }}"
+
+    - name: Printing final_excludes
+      debug:
+        var: final_excludes

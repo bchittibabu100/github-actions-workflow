f949cbc3e8979970f63b87404b06a95cfd4239e5
Chittibabu Boya <bchittibabu100@gmail.com>
2025-02-17 12:05:01 -0800

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,25 +1,29 @@
-#12 [ 3/16] RUN apk add --no-cache   ca-certificates   update-ca-certificates
-#12 0.198 fetch https://dl-cdn.alpinelinux.org/alpine/v3.20/main/x86_64/APKINDEX.tar.gz
-#12 0.281 28CB6691847F0000:error:0A000086:SSL routines:tls_post_process_server_certificate:certificate verify failed:ssl/statem/statem_clnt.c:2091:
-#12 0.282 WARNING: fetching https://dl-cdn.alpinelinux.org/alpine/v3.20/main: Permission denied
-#12 0.282 fetch https://dl-cdn.alpinelinux.org/alpine/v3.20/community/x86_64/APKINDEX.tar.gz
-#12 0.316 28CB6691847F0000:error:0A000086:SSL routines:tls_post_process_server_certificate:certificate verify failed:ssl/statem/statem_clnt.c:2091:
-#12 0.317 WARNING: fetching https://dl-cdn.alpinelinux.org/alpine/v3.20/community: Permission denied
-#12 0.317 ERROR: unable to select packages:
-#12 0.317   ca-certificates (no such package):
-#12 0.317     required by: world[ca-certificates]
-#12 0.317   update-ca-certificates (no such package):
-#12 0.317     required by: world[update-ca-certificates]
-#12 ERROR: process "/bin/sh -c apk add --no-cache   ca-certificates   update-ca-certificates" did not complete successfully: exit code: 2
+## Install IBM iAccessSeries app package
+RUN mkdir -p /opt/ibm
+COPY ./driver/ibm-iaccess-1.1.0.2-1.0.amd64.deb /opt/ibm/
+RUN dpkg -i *.deb
+RUN apt-get install -f
+COPY ./driver/config/* /etc/
+
+Here is the error log.....
+#10 [ 5/15] RUN mkdir -p /opt/ibm
+#10 DONE 0.4s
+#11 [ 6/15] COPY ./driver/ibm-iaccess-1.1.0.2-1.0.amd64.deb /opt/ibm/
+#11 DONE 0.0s
+#12 [ 7/15] RUN dpkg -i *.deb
+#12 0.180 /bin/sh: dpkg: not found
+#12 ERROR: process "/bin/sh -c dpkg -i *.deb" did not complete successfully: exit code: 127
 ------
- > [ 3/16] RUN apk add --no-cache   ca-certificates   update-ca-certificates:
-0.281 28CB6691847F0000:error:0A000086:SSL routines:tls_post_process_server_certificate:certificate verify failed:ssl/statem/statem_clnt.c:2091:
-0.282 WARNING: fetching https://dl-cdn.alpinelinux.org/alpine/v3.20/main: Permission denied
-0.282 fetch https://dl-cdn.alpinelinux.org/alpine/v3.20/community/x86_64/APKINDEX.tar.gz
-0.316 28CB6691847F0000:error:0A000086:SSL routines:tls_post_process_server_certificate:certificate verify failed:ssl/statem/statem_clnt.c:2091:
-0.317 WARNING: fetching https://dl-cdn.alpinelinux.org/alpine/v3.20/community: Permission denied
-0.317 ERROR: unable to select packages:
-0.317   ca-certificates (no such package):
-0.317     required by: world[ca-certificates]
-0.317   update-ca-certificates (no such package):
-0.317     required by: world[update-ca-certificates]
+ > [ 7/15] RUN dpkg -i *.deb:
+0.180 /bin/sh: dpkg: not found
+------
+Dockerfile:37
+--------------------
+  35 |     RUN mkdir -p /opt/ibm
+  36 |     COPY ./driver/ibm-iaccess-1.1.0.2-1.0.amd64.deb /opt/ibm/
+  37 | >>> RUN dpkg -i *.deb
+  38 |     RUN apt-get install -f
+  39 |     COPY ./driver/config/* /etc/
+--------------------
+ERROR: failed to solve: process "/bin/sh -c dpkg -i *.deb" did not complete successfully: exit code: 127
+Error: Process completed with exit code 1.

530be8bf8d6a87c22bd02f846a9ed30443c25e2a
Chittibabu Boya <bchittibabu100@gmail.com>
2025-04-07 21:19:38 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,3 +1,23 @@
-gitrunner@mo066inflrun03 ~ $git config --global --add safe.directory /home/gitrunner/actions-runner/_work/*/*
-error: wrong number of arguments, should be 2
-usage: git config [<options>]
+2025-04-08T04:14:40.2876182Z rm: cannot remove '/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Http/Abstractions/PlatformConfiguration/Dto/ClientBoardingConfigDto.cs': Permission denied
+2025-04-08T04:14:40.2879731Z rm: cannot remove '/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Http/Abstractions/PlatformConfiguration/Dto/EdiDeliveryFileExtensionEnumConverter.cs': Permission denied
+2025-04-08T04:14:40.2884300Z rm: cannot remove '/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Http/Abstractions/PlatformConfiguration/Dto/OtbBillingEntityDetailDto.cs': Permission denied
+2025-04-08T04:14:40.2887712Z rm: cannot remove '/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Http/Abstractions/PlatformConfiguration/Dto/PlatformConfigurationBoolConverter.cs': Permission denied
+2025-04-08T04:14:40.2891365Z rm: cannot remove '/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Http/Abstractions/PlatformConfiguration/Dto/OtbBillingEntityClientDto.cs': Permission denied
+2025-04-08T04:14:40.2894633Z rm: cannot remove '/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Http/Abstractions/PlatformConfiguration/Dto/OtbBillingEntityFieldDto.cs': Permission denied
+2025-04-08T04:14:40.2897856Z rm: cannot remove '/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing/src/VPay.DocSys.Parsing.Http/Abstractions/PlatformConfiguration/IPlatformConfigurationApi.cs': Permission denied
+
+
+gitrunner@mo066inflrun03 ~ $git config --global --get-all safe.directory
+/home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing
+gitrunner@mo066inflrun03 ~ $ll /home/gitrunner/actions-runner/_work/vpay-parsing/vpay-parsing
+total 36
+drwxr-xr-x  9 gitrunner docker 4096 Apr  7 23:14 ./
+drwxr-xr-x  3 gitrunner docker 4096 Apr  7 09:41 ../
+drwxr-xr-x  3 root      root   4096 Apr  7 09:41 charts/
+drwxr-xr-x  8 root      root   4096 Apr  7 15:49 .git/
+drwxr-xr-x  8 root      root   4096 Apr  7 09:41 .github/
+drwxr-xr-x  2 root      root   4096 Apr  7 09:41 .gitlab-ci/
+drwxr-xr-x 10 root      root   4096 Apr  7 09:41 src/
+drwxr-xr-x 10 root      root   4096 Apr  7 09:41 test/
+drwxr-xr-x  2 root      root   4096 Apr  7 09:41 wiki/
+gitrunner@mo066inflrun03 ~ $

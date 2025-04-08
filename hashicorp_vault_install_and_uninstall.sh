6360f70b266539322fb0f64125609d527f8ed02f
Chittibabu Boya <bchittibabu100@gmail.com>
2025-04-07 20:46:37 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,21 +1,3 @@
-$BAMBOO_HOME = "C:\Atlassian\Application Data\Bamboo"
-   $BACKUP_DIR = "C:\BambooConfigBackups"
-   $DATE = Get-Date -Format "yyyyMMdd"
-   $ZIP_PATH = "$BACKUP_DIR\bamboo_configs_$DATE.zip"
-
-   # Create backup dir if missing
-   if (!(Test-Path $BACKUP_DIR)) { New-Item -ItemType Directory -Path $BACKUP_DIR -Force }
-
-   # Backup only plan/job configs (build.xml files)
-   Compress-Archive -Path `
-     "$BAMBOO_HOME\xml-data\builds\*\build.xml", `
-     "$BAMBOO_HOME\xml-data\builds\*\*\build.xml" `
-     -DestinationPath $ZIP_PATH -Force
-
-   # Verify
-   Write-Host "Backup created at: $ZIP_PATH"
-   Get-ChildItem $ZIP_PATH
-
-   Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
-
-   
+gitrunner@mo066inflrun03 ~ $git config --global --add safe.directory /home/gitrunner/actions-runner/_work/*/*
+error: wrong number of arguments, should be 2
+usage: git config [<options>]

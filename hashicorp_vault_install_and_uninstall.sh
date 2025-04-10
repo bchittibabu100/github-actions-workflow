ca8cc6a483dabefdd586ea757b3700657a1b79b8
Chittibabu Boya <bchittibabu100@gmail.com>
2025-04-10 12:07:01 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,36 +1,39 @@
-inputSysNames=$1
-envName=$2
-orig=$3
-dest=$4
+jobs:
+  replace-vars:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout repository
+        uses: actions/checkout@v4
 
-# Split the sys names into array and process for each
-IFS=',' read -r -a sysArray <<< $inputSysNames
-for sys in "${sysArray[@]}"
-do
+      - name: Set environment variables
+        run: |
+          # Simulated environment variables (example)
+          export bamboo_vpay_FaxMan_prod_DBPassword='faxpass'
+          export bamboo_vpay_DocSys_prod_DBPassword='docpass'
+          export bamboo_vpay_FaxMan_prod_ApiKey='faxkey'
+          export bamboo_vpay_DocSys_prod_ApiKey='dockey'
 
-searchString="bamboo_vpay_(${sys})_${envName}_"
-# echo '----------------------------------------------------------------'
-# echo $searchString
-# echo '----------------------------------------------------------------'
-VARS=($(compgen -v | grep -P "${searchString}"))
-# echo '----------------------------------------------------------------'
-# cat $orig
-# echo '----------------------------------------------------------------'
-for i in "${VARS[@]}"
-do
-    #Remove $environ from variable name
-    var=`echo $i |  sed "s/bamboo_vpay_${sys}_${envName}_/${sys}/"`
-    #Replace variables in files
-	if [ `grep -c $var $orig` -gt 0 ]
-	then
-		echo "Replacing '$var' with '${!i}' in file '$file'"
-		sed -i "s|{{$var}}|${!i}|" $orig
-	fi 
-done
-done
-mv $orig $dest
+      - name: Replace template variables
+        run: |
+          inputSysNames="FaxMan,DocSys"
+          envName="prod"
+          orig="./appsettings.template-secure.json"
+          dest="./appsettings.secure.json"
 
-Argument: "FaxMan,DocSys" ${bamboo.vpay.DeployEnv} "./appsettings.template-secure.json" "./appsettings.secure.json"
+          IFS=',' read -r -a sysArray <<< "$inputSysNames"
 
-
-give me equivalent bash script to run them in github action workflow.
+          for sys in "${sysArray[@]}"
+          do
+            searchString="bamboo_vpay_${sys}_${envName}_"
+            VARS=($(compgen -v | grep -P "$searchString"))
+            
+            for i in "${VARS[@]}"
+            do
+              var=$(echo "$i" | sed "s/bamboo_vpay_${sys}_${envName}_/${sys}/")
+              if grep -q "$var" "$orig"; then
+                echo "Replacing '$var' with '${!i}' in file '$orig'"
+                sed -i "s|{{$var}}|${!i}|" "$orig"
+              fi
+            done
+          done
+          mv "$orig" "$dest"

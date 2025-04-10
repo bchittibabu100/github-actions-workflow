dd42fc28c9563157cf8f9e8b5d5f3a87ac6c6d50
Chittibabu Boya <bchittibabu100@gmail.com>
2025-04-10 12:04:06 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,13 +1,36 @@
+inputSysNames=$1
+envName=$2
+orig=$3
+dest=$4
 
-#!/bin/bash
+# Split the sys names into array and process for each
+IFS=',' read -r -a sysArray <<< $inputSysNames
+for sys in "${sysArray[@]}"
+do
 
-path="./"  # Adjust this to your base directory if needed
-
-find "$path" -type f -name "*.Tests.csproj" | while read -r file; do
-    name=$(basename "$file")
-    echo "$name"
-    dotnet test "$file" \
-        --no-restore \
-        --logger "trx;LogFileName=${bamboo_build_working_directory}/TestResults/${name}.trx" \
-        --filter "Category!=Integration"
+searchString="bamboo_vpay_(${sys})_${envName}_"
+# echo '----------------------------------------------------------------'
+# echo $searchString
+# echo '----------------------------------------------------------------'
+VARS=($(compgen -v | grep -P "${searchString}"))
+# echo '----------------------------------------------------------------'
+# cat $orig
+# echo '----------------------------------------------------------------'
+for i in "${VARS[@]}"
+do
+    #Remove $environ from variable name
+    var=`echo $i |  sed "s/bamboo_vpay_${sys}_${envName}_/${sys}/"`
+    #Replace variables in files
+	if [ `grep -c $var $orig` -gt 0 ]
+	then
+		echo "Replacing '$var' with '${!i}' in file '$file'"
+		sed -i "s|{{$var}}|${!i}|" $orig
+	fi 
 done
+done
+mv $orig $dest
+
+Argument: "FaxMan,DocSys" ${bamboo.vpay.DeployEnv} "./appsettings.template-secure.json" "./appsettings.secure.json"
+
+
+give me equivalent bash script to run them in github action workflow.

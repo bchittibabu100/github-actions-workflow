f1dddcb66d5387c06b5b3f5a1bd3dc732edf6501
Chittibabu Boya <bchittibabu100@gmail.com>
2025-04-15 19:22:55 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,39 +1,35 @@
-jobs:
-  replace-vars:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Checkout repository
-        uses: actions/checkout@v4
-
-      - name: Set environment variables
-        run: |
-          # Simulated environment variables (example)
-          export bamboo_vpay_FaxMan_prod_DBPassword='faxpass'
-          export bamboo_vpay_DocSys_prod_DBPassword='docpass'
-          export bamboo_vpay_FaxMan_prod_ApiKey='faxkey'
-          export bamboo_vpay_DocSys_prod_ApiKey='dockey'
-
-      - name: Replace template variables
-        run: |
-          inputSysNames="FaxMan,DocSys"
-          envName="prod"
-          orig="./appsettings.template-secure.json"
-          dest="./appsettings.secure.json"
-
-          IFS=',' read -r -a sysArray <<< "$inputSysNames"
-
-          for sys in "${sysArray[@]}"
-          do
-            searchString="bamboo_vpay_${sys}_${envName}_"
-            VARS=($(compgen -v | grep -P "$searchString"))
-            
-            for i in "${VARS[@]}"
-            do
-              var=$(echo "$i" | sed "s/bamboo_vpay_${sys}_${envName}_/${sys}/")
-              if grep -q "$var" "$orig"; then
-                echo "Replacing '$var' with '${!i}' in file '$orig'"
-                sed -i "s|{{$var}}|${!i}|" "$orig"
-              fi
-            done
-          done
-          mv "$orig" "$dest"
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: redis
+  labels:
+    app: redis
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: redis
+  template:
+    metadata:
+      labels:
+        app: redis
+    spec:
+      containers:
+        - name: redis
+          image: redis:alpine
+          ports:
+            - containerPort: 6379
+          resources:
+            limits:
+              memory: "256Mi"
+              cpu: "500m"
+          livenessProbe:
+            tcpSocket:
+              port: 6379
+            initialDelaySeconds: 5
+            periodSeconds: 10
+          readinessProbe:
+            tcpSocket:
+              port: 6379
+            initialDelaySeconds: 5
+            periodSeconds: 10

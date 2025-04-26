c017a93409a3357394fb20413c15df056e412bb6
Chittibabu Boya <bchittibabu100@gmail.com>
2025-04-25 21:23:24 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,8 +1,26 @@
-plprdlas-fax06 ~ # ps -ef | grep -Ei 'sssd|winbind'
-root      45665      1  0 23:01 ?        00:00:00 /usr/sbin/sssd -i --logger=files
-root      45666  45665  0 23:01 ?        00:00:00 /usr/libexec/sssd/sssd_be --domain kpay.net --uid 0 --gid 0 --logger=files
-root      45669  45665  0 23:01 ?        00:00:00 /usr/libexec/sssd/sssd_nss --uid 0 --gid 0 --logger=files
-root      45670  45665  0 23:01 ?        00:00:00 /usr/libexec/sssd/sssd_pam --uid 0 --gid 0 --logger=files
-
-plprdlas-fax06 ~ # auth required pam_access.so
--bash: auth: command not found
+(2025-04-25 23:10:17): [be[kPayUSA.net]] [pam_print_data] (0x0100): user: bamboosa@kpayusa.net
+(2025-04-25 23:10:17): [be[kPayUSA.net]] [pam_print_data] (0x0100): service: sshd
+(2025-04-25 23:10:17): [be[kPayUSA.net]] [pam_print_data] (0x0100): tty: ssh
+(2025-04-25 23:10:17): [be[kPayUSA.net]] [pam_print_data] (0x0100): ruser:
+(2025-04-25 23:10:17): [be[kPayUSA.net]] [pam_print_data] (0x0100): rhost: mo066inflrun01.kpayusa.net
+(2025-04-25 23:10:17): [be[kPayUSA.net]] [pam_print_data] (0x0100): authtok type: 0
+(2025-04-25 23:10:17): [be[kPayUSA.net]] [pam_print_data] (0x0100): newauthtok type: 0
+(2025-04-25 23:10:17): [be[kPayUSA.net]] [pam_print_data] (0x0100): priv: 1
+(2025-04-25 23:10:17): [be[kPayUSA.net]] [pam_print_data] (0x0100): cli_pid: 46307
+(2025-04-25 23:10:17): [be[kPayUSA.net]] [pam_print_data] (0x0100): logon name: not set
+(2025-04-25 23:11:11): [be[kPayUSA.net]] [dp_pam_handler] (0x0100): Got request with the following data
+(2025-04-25 23:11:11): [be[kPayUSA.net]] [pam_print_data] (0x0100): command: SSS_PAM_ACCT_MGMT
+(2025-04-25 23:11:11): [be[kPayUSA.net]] [pam_print_data] (0x0100): domain: kPayUSA.net
+(2025-04-25 23:11:11): [be[kPayUSA.net]] [pam_print_data] (0x0100): user: bamboosa@kpayusa.net
+(2025-04-25 23:11:11): [be[kPayUSA.net]] [pam_print_data] (0x0100): service: sshd
+(2025-04-25 23:11:11): [be[kPayUSA.net]] [pam_print_data] (0x0100): tty: ssh
+(2025-04-25 23:11:11): [be[kPayUSA.net]] [pam_print_data] (0x0100): ruser:
+(2025-04-25 23:11:11): [be[kPayUSA.net]] [pam_print_data] (0x0100): rhost: mo066inflrun01.kpayusa.net
+(2025-04-25 23:11:11): [be[kPayUSA.net]] [pam_print_data] (0x0100): authtok type: 0
+(2025-04-25 23:11:11): [be[kPayUSA.net]] [pam_print_data] (0x0100): newauthtok type: 0
+(2025-04-25 23:11:11): [be[kPayUSA.net]] [pam_print_data] (0x0100): priv: 1
+(2025-04-25 23:11:11): [be[kPayUSA.net]] [pam_print_data] (0x0100): cli_pid: 46356
+(2025-04-25 23:11:11): [be[kPayUSA.net]] [pam_print_data] (0x0100): logon name: not set
+(2025-04-25 23:12:20): [be[kPayUSA.net]] [dp_pam_handler] (0x0100): Got request with the following data
+(2025-04-25 23:12:20): [be[kPayUSA.net]] [pam_print_data] (0x0100): command: SSS_PAM_AUTHENTICATE
+(2025-04-25 23:12:20): [be[kPayUSA.net]] [pam_print_data] (0x0100): domain: VPayUSA.net

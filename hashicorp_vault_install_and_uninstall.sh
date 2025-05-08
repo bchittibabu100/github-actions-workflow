0c09a7a863f4b1f89a71fa3a18743f00323cb7fc
Chittibabu Boya <bchittibabu100@gmail.com>
2025-05-07 19:24:09 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,113 +1,20 @@
-Here is the content of values.yaml
+Here is the content of _metadata.yaml
 
-chart:
-  virtual:
-    charts:
-      - remittance-partner-network
-    shared:
-      generate:
-        deployment: true
-        service: true
-        ingress: false
-        secret: true
-        configmap: true
-      image:
-        registry: docker.repo1.hello.com/vpay-docker
-        tag: latest
-        pullPolicy: Always
+test@plinfldops02 ~/defaul_helm_templates/helm-gen/templates $ cat _metadata.tpl
+{{- /*
+Creates a standard metadata header with name and labels.
+A suffix can be passed in to adjust the name.
+*/ -}}
+{{- define "vpay.metadata" -}}
+  {{- $base := include "vpay.name" . -}}
+  {{- $suf := default "" .suffix -}}
+  {{- $name := printf "%s-%s" $base $suf -}}
+metadata:
+  name: {{ $name | lower | trimSuffix "-" }}
+  labels: {{ include "vpay.labels.standard" . | nindent 4 -}}
+{{- end -}}
 
-global:
-  environment:
-    name: dev
-
-rpn-api:
-  enabled: true
-  resources:
-    requests:
-      cpu: 1000m
-      memory: 2560Mi
-    limits:
-      cpu: 1000m
-      memory: 2560Mi
-  listeningPort: 8080
-  probes:
-    readiness:
-      endpoint: /actuator/health
-    livesness:
-      endpoint: /actuator/health
-  image:
-    name: optum/opay-remittance-partner-network
-  configMaps:
-    configmap:
-      mount: false
-      type: raw
-      data: |
-        remittance-configs.partner.serviceEndpoint: "one"
-        remittance-configs.partner.sendIndicator: "two"
-        remittance-configs.partner.b2bSendIndicator: "three"
-        remittance-configs.partner.clientKey: "four"
-        remittance-configs.partner.clientKeyValue: "five"
-        remittance-configs.partner.clientSecretKey: "six"
-        remittance-configs.partner.clientSecretKeyValue: "seven"
-        remittance-configs.partner.clientApiKey: "eight"
-        remittance-configs.partner.clientAuthEndpoint: "nine"
-        remittance-configs.partner.scopeValue: "ten"
-  env:
-    NODE_ENV: '{{ .Values.global.environment.name | lower }}'
-    server_environment: '{{ .Values.global.environment.name }}'
-    graylog_level: 'debug'
-    winston_silent_console: 'false'
-    graylog_handle_exceptions: 'true'
-    graylog_facility: remittance-partner-network
-    graylog_servers: '[{"host": "nonprod-syslog.vpayusa.net", "port": 5555}]'
-    injectedVars: |
-      - name: PARTNER_SERVICEENDPOINT
-        valueFrom:
-          configMapKeyRef:
-            name: remittance-partner-network-configmap
-            key: remittance-configs.partner.serviceEndpoint
-      - name: PARTNER_SEND_INDICATOR
-        valueFrom:
-          configMapKeyRef:
-            name: remittance-partner-network-configmap
-            key: remittance-configs.partner.sendIndicator
-      - name: PARTNER_B2B_SEND_INDICATOR
-        valueFrom:
-          configMapKeyRef:
-            name: remittance-partner-network-configmap
-            key: remittance-configs.partner.b2bSendIndicator
-      - name: PARTNER_CLIENTKEY
-        valueFrom:
-          configMapKeyRef:
-            name: remittance-partner-network-configmap
-            key: remittance-configs.partner.clientKey
-      - name: PARTNER_CLIENTKEYVALUE
-        valueFrom:
-          configMapKeyRef:
-            name: remittance-partner-network-configmap
-            key: remittance-configs.partner.clientKeyValue
-      - name: PARTNER_CLIENTSECRETKEY
-        valueFrom:
-          configMapKeyRef:
-            name: remittance-partner-network-configmap
-            key: remittance-configs.partner.clientSecretKey
-      - name: PARTNER_CLIENTSECRETKEYVALUE
-        valueFrom:
-          configMapKeyRef:
-            name: remittance-partner-network-configmap
-            key: remittance-configs.partner.clientSecretKeyValue
-      - name: PARTNER_CLIENTAPIKEY
-        valueFrom:
-          configMapKeyRef:
-            name: remittance-partner-network-configmap
-            key: remittance-configs.partner.clientApiKey
-      - name: PARTNER_CLIENTAUTHENDPOINT
-        valueFrom:
-          configMapKeyRef:
-            name: remittance-partner-network-configmap
-            key: remittance-configs.partner.clientAuthEndpoint
-      - name: PARTNER_SCOPEVALUE
-        valueFrom:
-          configMapKeyRef:
-            name: remittance-partner-network-configmap
-            key: remittance-configs.partner.scopeValue
+getting error as..
+2025-05-08T01:04:09.2210589Z helm upgrade remittance-partner-network . --install --namespace docsys-dev --wait --set-string chart.virtual.shared.image.tag="v1.0.1-devops2" --set-string chart.virtual.shared.env.RELEASE_COMMIT_SHA="7aafed3a2f9f40bedf2ccb85c09148986594a11e" --set-string chart.virtual.shared.env.RELEASE_TAG="v1.0.1-devops2" --set-string chart.virtual.shared.env.DEPLOYMENT_TIME="Thu\, 08 May 2025 01:04:08 +0000" --set-string chart.virtual.shared.env.DEPLOYMENT_DATACENTER="Plano" -f default-env-dc-values.yaml -f values-dev.yaml -f secret-values.yaml
+2025-05-08T01:04:09.3639961Z Release "remittance-partner-network" does not exist. Installing it now.
+2025-05-08T01:04:10.9570170Z Error: ConfigMap "remittance-partner-network-opay-remittance-partner-network-configmaps" is invalid: metadata.labels: Invalid value: "remittance-partner-network-remittance-partner-network-development": must be no more than 63 characters

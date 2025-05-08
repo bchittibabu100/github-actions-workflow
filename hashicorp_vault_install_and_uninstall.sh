e8eaf817c30da18db4acc21819282754aa369342
Chittibabu Boya <bchittibabu100@gmail.com>
2025-05-07 21:25:56 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,63 +1,66 @@
-Wanted to have have env section inside container like below then how should be the entries defined inside values.yaml ?
-
-  env:
-    - name: graylog_level
-      value: 'debug'
-    - name: winston_silent_console
-      value: 'false'
-    - name: graylog_handle_exceptions
-      value: 'true'
-    - name: graylog_facility
-      value: remittance-partner-network
-    - name: graylog_servers
-      value: '[{"host": "nonprod-syslog.vpayusa.net", "port": 5555}]'
-    - name: PARTNER_SERVICEENDPOINT
-      valueFrom:
-        configMapKeyRef:
-          name: remittance-partner-network-configmap
-          key: remittance-configs.partner.serviceEndpoint
-    - name: PARTNER_SEND_INDICATOR
-      valueFrom:
-        configMapKeyRef:
-          name: remittance-partner-network-configmap
-          key: remittance-configs.partner.sendIndicator
-    - name: PARTNER_B2B_SEND_INDICATOR
-      valueFrom:
-        configMapKeyRef:
-          name: remittance-partner-network-configmap
-          key: remittance-configs.partner.b2bSendIndicator
-    - name: PARTNER_CLIENTKEY
-      valueFrom:
-        configMapKeyRef:
-          name: remittance-partner-network-configmap
-          key: remittance-configs.partner.clientKey
-    - name: PARTNER_CLIENTKEYVALUE
-      valueFrom:
-        configMapKeyRef:
-          name: remittance-partner-network-configmap
-          key: remittance-configs.partner.clientKeyValue
-    - name: PARTNER_CLIENTSECRETKEY
-      valueFrom:
-        configMapKeyRef:
-          name: remittance-partner-network-configmap
-          key: remittance-configs.partner.clientSecretKey
-    - name: PARTNER_CLIENTSECRETKEYVALUE
-      valueFrom:
-        configMapKeyRef:
-          name: remittance-partner-network-configmap
-          key: remittance-configs.partner.clientSecretKeyValue
-    - name: PARTNER_CLIENTAPIKEY
-      valueFrom:
-        configMapKeyRef:
-          name: remittance-partner-network-configmap
-          key: remittance-configs.partner.clientApiKey
-    - name: PARTNER_CLIENTAUTHENDPOINT
-      valueFrom:
-        configMapKeyRef:
-          name: remittance-partner-network-configmap
-          key: remittance-configs.partner.clientAuthEndpoint
-    - name: PARTNER_SCOPEVALUE
-      valueFrom:
-        configMapKeyRef:
-          name: remittance-partner-network-configmap
-          key: remittance-configs.partner.scopeValue
+env:
+  graylog_level: debug
+  winston_silent_console: 'false'
+  graylog_handle_exceptions: 'true'
+  graylog_facility: remittance-partner-network
+  graylog_servers: '[{"host": "nonprod-syslog.vpayusa.net", "port": 5555}]'
+  PARTNER_SERVICEENDPOINT: |
+    {{- printf "" }}
+    valueFrom:
+      configMapKeyRef:
+        name: remittance-partner-network-configmap
+        key: remittance-configs.partner.serviceEndpoint
+  PARTNER_SEND_INDICATOR: |
+    {{- printf "" }}
+    valueFrom:
+      configMapKeyRef:
+        name: remittance-partner-network-configmap
+        key: remittance-configs.partner.sendIndicator
+  PARTNER_B2B_SEND_INDICATOR: |
+    {{- printf "" }}
+    valueFrom:
+      configMapKeyRef:
+        name: remittance-partner-network-configmap
+        key: remittance-configs.partner.b2bSendIndicator
+  PARTNER_CLIENTKEY: |
+    {{- printf "" }}
+    valueFrom:
+      configMapKeyRef:
+        name: remittance-partner-network-configmap
+        key: remittance-configs.partner.clientKey
+  PARTNER_CLIENTKEYVALUE: |
+    {{- printf "" }}
+    valueFrom:
+      configMapKeyRef:
+        name: remittance-partner-network-configmap
+        key: remittance-configs.partner.clientKeyValue
+  PARTNER_CLIENTSECRETKEY: |
+    {{- printf "" }}
+    valueFrom:
+      configMapKeyRef:
+        name: remittance-partner-network-configmap
+        key: remittance-configs.partner.clientSecretKey
+  PARTNER_CLIENTSECRETKEYVALUE: |
+    {{- printf "" }}
+    valueFrom:
+      configMapKeyRef:
+        name: remittance-partner-network-configmap
+        key: remittance-configs.partner.clientSecretKeyValue
+  PARTNER_CLIENTAPIKEY: |
+    {{- printf "" }}
+    valueFrom:
+      configMapKeyRef:
+        name: remittance-partner-network-configmap
+        key: remittance-configs.partner.clientApiKey
+  PARTNER_CLIENTAUTHENDPOINT: |
+    {{- printf "" }}
+    valueFrom:
+      configMapKeyRef:
+        name: remittance-partner-network-configmap
+        key: remittance-configs.partner.clientAuthEndpoint
+  PARTNER_SCOPEVALUE: |
+    {{- printf "" }}
+    valueFrom:
+      configMapKeyRef:
+        name: remittance-partner-network-configmap
+        key: remittance-configs.partner.scopeValue

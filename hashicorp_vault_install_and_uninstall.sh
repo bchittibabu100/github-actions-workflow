0cf6951105de6616480b9f7baa8778189cd701d1
Chittibabu Boya <bchittibabu100@gmail.com>
2025-05-07 21:10:27 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,96 +1,142 @@
-test@plinfldops02 ~/defaul_helm_templates/helm-gen/templates $ cat _podmetaspec.tpl
-{{- define "vpay.podmetaspec.tpl" -}}
+test@plinfldops02 ~/defaul_helm_templates/helm-gen/templates $ cat _container.tpl
+{{- define "vpay.container.tpl" -}}
+  {{- $probes := default (dict) .Values.probes -}}
+  {{- $liveness := default (dict) $probes.liveness -}}
+  {{- $readiness := default (dict) $probes.readiness -}}
+  {{- $primaryPort := ternary (int (dig "experimentalFeatures" "exposeHttpAndHttps" "httpsPort" (443) .Values)) (int (.Values.listeningPort)) (dig "experimentalFeatures" "exposeHttpAndHttps" "enabled" (false) .Values) -}}
+  {{- $portName := default "primary-port" .Values.portName -}}
+  {{- $portScheme := ternary ("HTTPS") (default "HTTP" .Values.portScheme) (dig "experimentalFeatures" "exposeHttpAndHttps" "enabled" (false) .Values) -}}
+  {{- $imageWithTag := printf "%s:%s" .Values.image.name (.Values.image.tag | toString) -}}
+  {{- $startup := $probes.startup -}}
+name: {{ default (include "vpay.name" .) .Values.containerName }}
+image: {{ ternary ($imageWithTag) (printf "%s/%s" .Values.image.registry $imageWithTag) (eq (.Values.image.registry | toString) "<nil>") }}
+imagePullPolicy: {{ default "Always" .Values.image.pullPolicy}}
+env:
+  - name: K8S_NODE_NAME
+    valueFrom:
+      fieldRef:
+        fieldPath: spec.nodeName
+  - name: K8S_POD_NAME
+    valueFrom:
+      fieldRef:
+        fieldPath: metadata.name
 
-  {{- $hasMountedSecrets := false -}}
-  {{- range $key, $value := default (dict) .Values.secrets -}}
-    {{- if (include "vpay.util.trueenabled" $value.mount) -}}
-      {{- $hasMountedSecrets = true -}}
-    {{- end -}}
-  {{- end -}}
-  {{- $hasMountedConfigMaps := false -}}
-  {{- range $key, $value := default (dict) .Values.configMaps -}}
-    {{- if (include "vpay.util.trueenabled" $value.mount) -}}
-      {{- $hasMountedConfigMaps = true -}}
-    {{- end -}}
-  {{- end -}}
+  {{- if .Values.env }}
+    {{- range $key, $val := .Values.env }}
+    {{- if (eq ($val | toString) "<nil>" | not) }}
+  - name: {{ $key }}
+    value: {{ tpl ($val | toString) $ | quote }}
+    {{- end }}
+    {{- end }}
+  {{- end }}
 
-metadata:
-  labels: {{ include "vpay.labels.standard" . | nindent 4 }}
-{{- if (or .Values.secretEnv $hasMountedSecrets $hasMountedConfigMaps) }}
-  annotations:
-  {{- if (.Values.secretEnv) }}
-    {{ include "vpay.secret.secretEnv.checksums" . | indent 4 | trim }}
-  {{- end }}
-  {{- if $hasMountedSecrets }}
-    {{ include "vpay.secret.checksums" . | indent 4 | trim }}
-  {{- end }}
-  {{- if $hasMountedConfigMaps }}
-    {{ include "vpay.configmap.checksums" . | indent 4 | trim }}
-  {{- end }}
+{{- if .Values.secretEnv }}
+envFrom:
+  - secretRef:
+      name: {{ default (include "vpay.name" .) .Values.containerName }}-env
+      optional: false
 {{- end }}
-spec:
-  containers:
-    - {{ include "vpay.container.tpl" . | indent 6 | trim }}
-      {{- range $key, $val := default (dict) .Values.sidecar }}
-      {{- $r := deepCopy $ }}
-      {{- $portName := printf "%s-port" $key -}}
-      {{- $_ := unset (unset (unset (unset (unset $r.Values "resources") "secretEnv") "serviceExposePort") "listeningPort") "probes" -}}
-      {{- if (.secretEnv) }}
-        {{- fail "secretEnv can only be set on the primary container; it is not currently supported in sidecar containers." }}
+
+  {{- if (dig "experimentalFeatures" "exposeHttpAndHttps" "enabled" (false) .Values) }}
+    {{- if (or (.Values.listeningPort) (.Values.serviceExposePort) (.Values.portScheme)) }}
+      {{- fail "When 'exposeHttpAndHttps' is enabled, you can't provide a 'listeningPort', 'portScheme', or 'serviceExposePort'" }}
+    {{- end }}
+    {{- if (eq (int (dig "experimentalFeatures" "exposeHttpAndHttps" "httpsPort" (443) .Values)) (int (dig "experimentalFeatures" "exposeHttpAndHttps" "httpPort" (80) .Values))) }}
+      {{- fail "When 'exposeHttpAndHttps' is enabled, the HTTPS and HTTP ports must be different" }}
+    {{- end }}
+  {{- end }}
+  {{- if $primaryPort }}
+ports:
+  - name: {{ $portName }}
+    containerPort: {{ $primaryPort }}
+    protocol: TCP
+  {{- if (dig "experimentalFeatures" "exposeHttpAndHttps" "enabled" (false) .Values) }}
+  - name: http-port
+    containerPort: {{ int (dig "experimentalFeatures" "exposeHttpAndHttps" "httpPort" (80) .Values) }}
+    protocol: TCP
+  {{- end }}
+    {{- if include "vpay.util.trueenabled" $liveness.enabled }}
+livenessProbe:
+      {{- if $liveness.endpoint }}
+  httpGet:
+    path: {{ $liveness.endpoint }}
+    port: {{ $portName }}
+    {{- if (eq $portScheme "HTTP" | not) }}
+    scheme: {{ $portScheme }}
+    {{- end }}
+      {{- else }}
+  tcpSocket:
+    port: {{ $portName }}
       {{- end }}
-      {{- $_ := mergeOverwrite $r (set (dict) "Values" (set (set . "portName" $portName) "containerName" $key)) }}
-    - {{ include "vpay.container.tpl" $r | indent 6 | trim }}
+  initialDelaySeconds: {{ default (30) $liveness.initialDelaySeconds }}
+  periodSeconds: {{ default (30) $liveness.periodSeconds }}
+  failureThreshold: {{ default (5) $liveness.failureThreshold }}
+  successThreshold: {{ default (1) $startup.successThreshold }}
+  timeoutSeconds: {{ default (3) $liveness.timeoutSeconds }}
+    {{- end }}
+    {{- if include "vpay.util.trueenabled" $readiness.enabled }}
+readinessProbe:
+      {{- if $readiness.endpoint }}
+  httpGet:
+    path: {{ $readiness.endpoint }}
+    port: {{ $portName }}
+    {{- if (eq $portScheme "HTTP" | not) }}
+    scheme: {{ $portScheme }}
+    {{- end }}
+      {{- else }}
+  tcpSocket:
+    port: {{ $portName }}
       {{- end }}
-  {{- if (dig "experimentalFeatures" "podAntiAffinity" "enabled" (true) .Values) }}
-  affinity:
-    podAntiAffinity:
-      preferredDuringSchedulingIgnoredDuringExecution:
-        - weight: 100
-          podAffinityTerm:
-            labelSelector:
-              matchExpressions:
-                - key: "app.kubernetes.io/name"
-                  operator: In
-                  values:
-                    - {{ template "vpay.name" . }}
-                - key: "app.kubernetes.io/instance"
-                  operator: In
-                  values:
-                    - {{ template "vpay.name.full" . }}
-            topologyKey: "kubernetes.io/hostname"
-  {{- end }}
-  {{- if .hasMountedVolumes }}
-  volumes:
-    {{- if (include "vpay.util.anymounted.secret" .Values.secrets) }}
-      {{- include "vpay.secret.volumes" . | trim | nindent 4 }}
+  periodSeconds: {{ default (10) $readiness.periodSeconds }}
+  failureThreshold: {{ default (3) $readiness.failureThreshold }}
+  successThreshold: {{ default (1) $startup.successThreshold }}
+  timeoutSeconds: {{ default (3) $readiness.timeoutSeconds }}
     {{- end }}
-    {{- if (include "vpay.util.anymounted.secret" .Values.configMaps) }}
-      {{- include "vpay.configmap.volumes" . | trim | nindent 4 }}
+    {{- if (not (eq ($startup | toString) "<nil>")) }}
+      {{- if include "vpay.util.trueenabled" $startup.enabled }}
+startupProbe:
+        {{- if $startup.endpoint }}
+  httpGet:
+    path: {{ $startup.endpoint }}
+    port: {{ $portName }}
+    {{- if (eq $portScheme "HTTP" | not) }}
+    scheme: {{ $portScheme }}
     {{- end }}
-    {{- if (include "vpay.util.anymounted.share" (dict "mountPoints" .Values.mountNfs "shares" $.__GlobalValues.nfsShares)) }}
-      {{- include "vpay.pvc.nfs.volumes" . | trim | nindent 4 }}
+        {{- else }}
+  tcpSocket:
+    port: {{ $portName }}
+        {{- end }}
+  periodSeconds: {{ default (10) $startup.periodSeconds }}
+  failureThreshold: {{ default (5) $startup.failureThreshold }}
+  successThreshold: {{ default (1) $startup.successThreshold }}
+  timeoutSeconds: {{ default (3) $startup.timeoutSeconds }}
+      {{- end }}
     {{- end }}
-    {{- if (include "vpay.util.anymounted.share" (dict "mountPoints" .Values.mountSmb "shares" $.__GlobalValues.smbShares)) }}
-      {{- include "vpay.pvc.smb.volumes" . | trim | nindent 4 }}
-    {{- end }}
-    {{- if (include "vpay.util.anymounted.share" (dict "mountPoints" .Values.mountEmptyDir "shares" (dict))) }}
-      {{- include "vpay.emptyDir.volumes" . | trim | nindent 4 }}
-    {{- end }}
-    {{- if (include "vpay.util.anymounted.share" (dict "mountPoints" .Values.mountPersistentVolumeClaims "shares" (dict))) }}
-      {{- include "vpay.pvc-raw.volumes" . | trim | nindent 4 }}
-    {{- end }}
-  {{- end }}
-  {{- if .Values.serviceAccountName }}
-  serviceAccountName: {{ .Values.serviceAccountName }}
-  {{- end }}
-  {{- if not (eq (.Values.podActiveDeadlineSeconds | toString) "<nil>") }}
-  activeDeadlineSeconds: {{ int .Values.podActiveDeadlineSeconds }}
-  {{- end }}
-  {{- if not (eq (.Values.restartPolicy | toString) "<nil>") }}
-  restartPolicy: {{ .Values.restartPolicy }}
-  {{- end }}
-  {{- if not (eq (.Values.terminationGracePeriodSeconds | toString) "<nil>") }}
-  terminationGracePeriodSeconds: {{ int .Values.terminationGracePeriodSeconds }}
   {{- end }}
 
-{{- end -}}
+  {{- if .Values.resources }}
+resources: {{- .Values.resources | toYaml | trim | nindent 2 }}
+  {{- end }}
+
+  {{- if .hasMountedVolumes }}
+volumeMounts:
+  {{- if (include "vpay.util.anymounted.secret" .Values.secrets) }}
+    {{- include "vpay.secret.volumeMounts" . | trim | nindent 2 }}
+  {{- end }}
+  {{- if (include "vpay.util.anymounted.secret" .Values.configMaps) }}
+    {{- include "vpay.configmap.volumeMounts" . | trim | nindent 2 }}
+  {{- end }}
+  {{- if (include "vpay.util.anymounted.share" (dict "mountPoints" .Values.mountNfs "shares" $.__GlobalValues.nfsShares)) }}
+    {{- include "vpay.pvc.nfs.volumeMounts" . | trim | nindent 2 }}
+  {{- end }}
+  {{- if (include "vpay.util.anymounted.share" (dict "mountPoints" .Values.mountSmb "shares" $.__GlobalValues.smbShares)) }}
+    {{- include "vpay.pvc.smb.volumeMounts" . | trim | nindent 2 }}
+  {{- end }}
+  {{- if (include "vpay.util.anymounted.share" (dict "mountPoints" .Values.mountEmptyDir "shares" (dict))) }}
+    {{- include "vpay.emptyDir.volumeMounts" . | trim | nindent 2 }}
+  {{- end }}
+  {{- if (include "vpay.util.anymounted.share" (dict "mountPoints" .Values.mountPersistentVolumeClaims "shares" (dict))) }}
+    {{- include "vpay.pvc-raw.volumeMounts" . | trim | nindent 2 }}
+  {{- end }}
+  {{- end -}}
+{{- end }}

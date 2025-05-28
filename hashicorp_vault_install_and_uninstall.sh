02fb49ebcc92c3c00c6d53e4857cac22ae0fdb15
Chittibabu Boya <bchittibabu100@gmail.com>
2025-05-27 20:38:18 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,12 +1,88 @@
-apiVersion: v2
-name: rpn-partner-network
-description: rpn network
-version: 0.0.1
-dependencies:
-  - name: helm-gen
-    version: v1.2.0
-    repository: https://stlinfharbor.test.net/chartrepo/devops
+Here is the pv manifest
 
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  annotations:
+    meta.helm.sh/release-name: eftfeepull
+    meta.helm.sh/release-namespace: eftfeepull-smb-test
+  finalizers:
+  - kubernetes.io/pv-protection
+  labels:
+    app: eftfeepull-eftfeepull-api-production
+    app.kubernetes.io/cluster-domain: prod.pks.test.net
+    app.kubernetes.io/instance: eftfeepull-eftfeepull-api
+    app.kubernetes.io/managed-by: Helm
+    app.kubernetes.io/name: eftfeepull-api
+    app.kubernetes.io/part-of: eftfeepull-api
+    app.vpayusa.com/smb-name: eftfeepull-smb-test
+    app.vpayusa.com/type: smb-server
+    helm.sh/chart: eftfeepull-api-0.0.1
+  name: eftfeepull-smb-test
+spec:
+  accessModes:
+  - ReadWriteMany
+  capacity:
+    storage: 5000Gi
+  claimRef:
+    apiVersion: v1
+    kind: PersistentVolumeClaim
+    name: eftfeepull-pvc-test
+    namespace: eftfeepull-smb-test
+  csi:
+    driver: smb.csi.k8s.io
+    nodeStageSecretRef:
+      name: eftfeepull-api-bogner-rw
+      namespace: eftfeepull-smb-test
+    volumeAttributes:
+      source: //plprdwsmb03.test.net/scrushftp
+    volumeHandle: eftfeepull-api-bogner-rw-v1
+  mountOptions:
+  - dir_mode=0777
+  - file_mode=0777
+  - vers=3.0
+  - noperm
+  - mfsymlinks
+  persistentVolumeReclaimPolicy: Retain
+  storageClassName: smb
+  volumeMode: Filesystem
+  
+  
+Here is the pvc manifest
 
-This is the content of Chart.yaml and this helm-gen-v1.2.0.tgz exists in current folder where values files exist.
-How can I execute dry-runs locally before running it through github action workflow.
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  annotations:
+    meta.helm.sh/release-name: eftfeepull-pvc-test
+    meta.helm.sh/release-namespace: eftfeepull-test-crushftp
+  finalizers:
+  - kubernetes.io/pvc-protection
+  labels:
+    app: eftfeepull-eftfeepull-api-production
+    app.kubernetes.io/cluster-domain: prod.pks.test.net
+    app.kubernetes.io/instance: eftfeepull-smb-test
+    app.kubernetes.io/managed-by: Helm
+    app.kubernetes.io/name: eftfeepull-api
+    app.kubernetes.io/part-of: eftfeepull-api
+    app.vpayusa.com/smb-name: eftfeepull-smb-test
+    app.vpayusa.com/type: smb-server
+    helm.sh/chart: eftfeepull-api-0.0.1
+  name: eftfeepull-pvc-test
+  namespace: eftfeepull-test-crushftp
+spec:
+  accessModes:
+  - ReadWriteMany
+  resources:
+    requests:
+      storage: 5000Gi
+  selector:
+    matchLabels:
+      app.vpayusa.com/smb-name: eftfeepull-api-eft-submit-rw-share
+      app.vpayusa.com/type: smb-server
+  storageClassName: smb
+  volumeMode: Filesystem
+  volumeName: eftfeepull-smb-test
+  
+Here is the events of pvc
+12s         Warning   FailedBinding   persistentvolumeclaim/eftfeepull-pvc-test                  volume "eftfeepull-smb-test" already bound to a different claim.

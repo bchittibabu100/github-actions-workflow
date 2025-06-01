ed153bf25ee9391c6420f2e1d2e5a4f41b3f4165
Chittibabu Boya <bchittibabu100@gmail.com>
2025-05-31 21:44:17 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,2 +1,10 @@
-pod is failing to come up.
-Warning  Failed     9s    kubelet            Failed to pull image "nginx-docker-prod.repo1.test.com/vpay-docker/base-images/nginx-1-17:commit-master": rpc error: code = Unknown desc = Error response from daemon: unknown: {"errors":[{"code":"DENIED","message":"Artifact download request rejected: test-docker/base-images/nginx-1-17/commit-master/manifest.json was not downloaded due to the download blocking policy configured in Xray for nginx-docker-prod.","detail":{"manifest":"test-docker/base-images/nginx-1-17/commit-master"}}]}
+  Warning  FailedMount  14s   kubelet            MountVolume.SetUp failed for volume "eftfeepull-api-eft-submit-rw-share" : rpc error: code = Internal desc = Could not mount "/var/vcap/data/kubelet/plugins/kubernetes.io/csi/pv/eftfeepull-api-eft-submit-rw-share/globalmount" at "/var/vcap/data/kubelet/pods/3abcf045-7fe8-40ed-b423-fd25c3c101fb/volumes/kubernetes.io~csi/eftfeepull-api-eft-submit-rw-share/mount": mount failed: exit status 32
+Mounting command: mount
+Mounting arguments:  -o bind /var/vcap/data/kubelet/plugins/kubernetes.io/csi/pv/eftfeepull-api-eft-submit-rw-share/globalmount /var/vcap/data/kubelet/pods/3abcf045-7fe8-40ed-b423-fd25c3c101fb/volumes/kubernetes.io~csi/eftfeepull-api-eft-submit-rw-share/mount
+Output: mount: /var/vcap/data/kubelet/pods/3abcf045-7fe8-40ed-b423-fd25c3c101fb/volumes/kubernetes.io~csi/eftfeepull-api-eft-submit-rw-share/mount: special device /var/vcap/data/kubelet/plugins/kubernetes.io/csi/pv/eftfeepull-api-eft-submit-rw-share/globalmount does not exist.
+
+
+  Warning  FailedMount  0s (x5 over 14s)  kubelet  MountVolume.SetUp failed for volume "eftfeepull-api-bogner-rw-v1" : rpc error: code = Internal desc = Could not mount "/var/vcap/data/kubelet/plugins/kubernetes.io/csi/pv/eftfeepull-api-bogner-rw-v1/globalmount" at "/var/vcap/data/kubelet/pods/3abcf045-7fe8-40ed-b423-fd25c3c101fb/volumes/kubernetes.io~csi/eftfeepull-api-bogner-rw-v1/mount": mount failed: exit status 32
+Mounting command: mount
+Mounting arguments:  -o bind /var/vcap/data/kubelet/plugins/kubernetes.io/csi/pv/eftfeepull-api-bogner-rw-v1/globalmount /var/vcap/data/kubelet/pods/3abcf045-7fe8-40ed-b423-fd25c3c101fb/volumes/kubernetes.io~csi/eftfeepull-api-bogner-rw-v1/mount
+Output: mount: /var/vcap/data/kubelet/pods/3abcf045-7fe8-40ed-b423-fd25c3c101fb/volumes/kubernetes.io~csi/eftfeepull-api-bogner-rw-v1/mount: special device /var/vcap/data/kubelet/plugins/kubernetes.io/csi/pv/eftfeepull-api-bogner-rw-v1/globalmount does not exist.

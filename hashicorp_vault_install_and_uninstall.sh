ebbb14f6c7b5956f82994267a11d3ba9f0962eff
Chittibabu Boya <bchittibabu100@gmail.com>
2025-06-12 21:32:42 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,13 +1,9 @@
-{
-  "id": "CVE-2024-51501_refit_7.2.1",
-  "shortDescription": {
-    "text": "[CVE-2024-51501] refit 7.2.1"
-  },
-  "help": {
-    "text": "Refit is an automatic type-safe REST library for .NET Core, Xamarin and .NET The various header-related Refit attributes (Header, HeaderCollection and Authorize) are vulnerable to CRLF injection. The way HTTP headers are added to a request is via the `HttpHeaders.TryAddWithoutValidation` method. This method does not check for CRLF characters in the header value. This means that any headers added to a refit request are vulnerable to CRLF-injection. In general, CRLF-injection into a HTTP header (when using HTTP/1.1) means that one can inject additional HTTP headers or smuggle whole HTTP requests. If an application using the Refit library passes a user-controllable value through to a header, then that application becomes vulnerable to CRLF-injection. This is not necessarily a security issue for a command line application like the one above, but if such code were present in a web application then it becomes vulnerable to request splitting (as shown in the PoC) and thus Server Side Request
-    "markdown": "| Severity Score | Direct Dependencies | Fixed Versions     |\n| :---:        |    :----:   |          :---: |\n| 9.8      | `sha256__4110e4a8866f596ee3b4aea66c831f6ba5d5aad4060e17a3d4faaa5670b0f7d5.tar `       | [7.2.22]   |"
-  },
-  "properties": {
-    "security-severity": "9.8"
-  }
-}
+stlstglvault1 vault # ll /backup/stg_vault/pl_stg_vault/
+total 1920
+-rwxrwxrwx. 1 smbstgpks smb_stg_service_rw 467031 Jun 11 16:00 vault_backup_20250611160001.snap
+-rwxrwxrwx. 1 smbstgpks smb_stg_service_rw 467034 Jun 12 00:00 vault_backup_20250612000001.snap
+-rwxrwxrwx. 1 smbstgpks smb_stg_service_rw 467037 Jun 12 08:00 vault_backup_20250612080001.snap
+-rwxrwxrwx. 1 smbstgpks smb_stg_service_rw 467032 Jun 12 16:00 vault_backup_20250612160001.snap
+
+
+There will be hashicorp vault backup will be created once in every 4 hours. we will have to create a bash script to restore snapshot and schedule it via cron.

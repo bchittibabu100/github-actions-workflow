e161f8360e6598f41ecd04d5de2c8fcbd5b295df
Chittibabu Boya <bchittibabu100@gmail.com>
2025-07-07 16:55:17 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,4 +1,8 @@
-cat awx-deploy.yaml                                                                                                                                                  ─╯
+kubectl apply -f awx-deploy.yaml
+Error from server (BadRequest): error when creating "awx-deploy.yaml": AWX in version "v1beta1" cannot be handled as a AWX: strict decoding error: unknown field "spec.pod_spec_override"
+
+
+cat awx-deploy.yaml
 ---
 apiVersion: awx.ansible.com/v1beta1
 kind: AWX
@@ -8,16 +12,13 @@ metadata:
 spec:
   service_type: ClusterIP
   ingress_type: none
-  node_selector:
-    workload: awx
-  tolerations:
-    - key: "awx"
-      operator: "Equal"
-      value: "true"
-      effect: "NoSchedule"
+  pod_spec_override: |
+    nodeSelector:
+      workload: awx
+    tolerations:
+      - key: "awx"
+        operator: "Equal"
+        value: "true"
+        effect: "NoSchedule"
   postgres_storage_class: managed-premium
   redis_image: redis:7.0
-
-
-kubectl apply -f awx-deploy.yaml
-Error from server (BadRequest): error when creating "awx-deploy.yaml": AWX in version "v1beta1" cannot be handled as a AWX: strict decoding error: unknown field "spec.node_selector.workload", unknown field "spec.tolerations[0].effect", unknown field "spec.tolerations[0].key", unknown field "spec.tolerations[0].operator", unknown field "spec.tolerations[0].value"

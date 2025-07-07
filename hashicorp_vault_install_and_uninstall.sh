a01687486e7a2f726da1b6dbf22c2905d58b3e79
Chittibabu Boya <bchittibabu100@gmail.com>
2025-07-07 16:47:43 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,9 +1,23 @@
-Error from server (Forbidden): error when creating "STDIN": serviceaccounts "awx-operator-controller-manager" is forbidden: unable to create new content in namespace awx because it is being terminated
-Error from server (Forbidden): error when creating "STDIN": roles.rbac.authorization.k8s.io "awx-operator-awx-manager-role" is forbidden: unable to create new content in namespace awx because it is being terminated
-Error from server (Forbidden): error when creating "STDIN": roles.rbac.authorization.k8s.io "awx-operator-leader-election-role" is forbidden: unable to create new content in namespace awx because it is being terminated
-Error from server (Forbidden): error when creating "STDIN": rolebindings.rbac.authorization.k8s.io "awx-operator-awx-manager-rolebinding" is forbidden: unable to create new content in namespace awx because it is being terminated
-Error from server (Forbidden): error when creating "STDIN": rolebindings.rbac.authorization.k8s.io "awx-operator-leader-election-rolebinding" is forbidden: unable to create new content in namespace awx because it is being terminated
-Error from server (Forbidden): error when creating "STDIN": configmaps "awx-operator-awx-manager-config" is forbidden: unable to create new content in namespace awx because it is being terminated
-Error from server (Forbidden): error when creating "STDIN": services "awx-operator-controller-manager-metrics-service" is forbidden: unable to create new content in namespace awx because it is being terminated
-Error from server (Forbidden): error when creating "STDIN": deployments.apps "awx-operator-controller-manager" is forbidden: unable to create new content in namespace awx because it is being terminated
-make: *** [deploy] Error 1
+cat awx-deploy.yaml                                                                                                                                                  ─╯
+---
+apiVersion: awx.ansible.com/v1beta1
+kind: AWX
+metadata:
+  name: awx
+  namespace: awx
+spec:
+  service_type: ClusterIP
+  ingress_type: none
+  node_selector:
+    workload: awx
+  tolerations:
+    - key: "awx"
+      operator: "Equal"
+      value: "true"
+      effect: "NoSchedule"
+  postgres_storage_class: managed-premium
+  redis_image: redis:7.0
+
+
+kubectl apply -f awx-deploy.yaml
+Error from server (BadRequest): error when creating "awx-deploy.yaml": AWX in version "v1beta1" cannot be handled as a AWX: strict decoding error: unknown field "spec.node_selector.workload", unknown field "spec.tolerations[0].effect", unknown field "spec.tolerations[0].key", unknown field "spec.tolerations[0].operator", unknown field "spec.tolerations[0].value"

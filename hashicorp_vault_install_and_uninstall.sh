15d7489eeda165b80197a1fbb8b1c68612c0707b
Chittibabu Boya <bchittibabu100@gmail.com>
2025-07-07 17:23:17 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,7 +1,7 @@
-kubectl apply -f awx-deploy.yaml
-Error from server (BadRequest): error when creating "awx-deploy.yaml": AWX in version "v1beta1" cannot be handled as a AWX: strict decoding error: unknown field "spec.pod_spec_override"
+Error from server (BadRequest): error when creating "awx-deploy.yaml": AWX in version "v1beta1" cannot be handled as a AWX: strict decoding error: unknown field "spec.task_node_selector.workload", unknown field "spec.task_tolerations[0].effect", unknown field "spec.task_tolerations[0].key", unknown field "spec.task_tolerations[0].operator", unknown field "spec.task_tolerations[0].value", unknown field "spec.web_node_selector.workload", unknown field "spec.web_tolerations[0].effect", unknown field "spec.web_tolerations[0].key", unknown field "spec.web_tolerations[0].operator", unknown field "spec.web_tolerations[0].value"
 
-
+current tag:
+2.16.0
 cat awx-deploy.yaml
 ---
 apiVersion: awx.ansible.com/v1beta1
@@ -12,13 +12,19 @@ metadata:
 spec:
   service_type: ClusterIP
   ingress_type: none
-  pod_spec_override: |
-    nodeSelector:
-      workload: awx
-    tolerations:
-      - key: "awx"
-        operator: "Equal"
-        value: "true"
-        effect: "NoSchedule"
   postgres_storage_class: managed-premium
   redis_image: redis:7.0
+  task_node_selector:
+    workload: awx
+  web_node_selector:
+    workload: awx
+  task_tolerations:
+    - key: "awx"
+      operator: "Equal"
+      value: "true"
+      effect: "NoSchedule"
+  web_tolerations:
+    - key: "awx"
+      operator: "Equal"
+      value: "true"
+      effect: "NoSchedule"

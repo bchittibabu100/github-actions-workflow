99c35ac45fb04a3cb44685a7f476fb6e622ba5f6
Chittibabu Boya <bchittibabu100@gmail.com>
2025-07-09 11:43:08 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -64,13 +64,7 @@ kubectl get secret awx-demo-admin-password -n awx -o jsonpath="{.data.password}"
 
 
 
-*********************\r\ntask path: /opt/ansible/roles/installer/tasks/resources_configuration.yml:233\nok: [localhost] => {\"ansible_facts\": {\"_default_redis_image\": \"docker.io/redis:7\"}, \"changed\": false}\n\r\nTASK [installer : Set user provided redis image] *******************************\r\ntask path: /opt/ansible/roles/installer/tasks/resources_configuration.yml:237\nfatal: [localhost]: FAILED! => {\"msg\": \"The conditional check 'redis_image_version is defined or redis_image_version != ''' failed. The error was: error while evaluating conditional (redis_image_version is defined or redis_image_version != ''): 'redis_image_version' is undefined. 'redis_image_version' is undefined\\n\\nThe error appears to be in '/opt/ansible/roles/installer/tasks/resources_configuration.yml': line 237, column 3, but may\\nbe elsewhere in the file depending on the exact syntax problem.\\n\\nThe offending line appears to be:\\n\\n\\n- name: Set user provided redis image\\n  ^ here\\n\"}\n\r\nPLAY RECAP *********************************************************************\r\nlocalhost                  : ok=61   changed=0    unreachable=0    failed=1    skipped=68   rescued=0    ignored=0   \n","job":"3334994276792168154","name":"awx-ansible","namespace":"awx","error":"exit status 2","stacktrace":"github.com/operator-framework/ansible-operator-plugins/internal/ansible/runner.(*runner).Run.func1\n\tansible-operator-plugins/internal/ansible/runner/runner.go:269"}
-
------ Ansible Task Status Event StdOut (awx.ansible.com/v1beta1, Kind=AWX, awx-ansible/awx) -----
-
-
-PLAY RECAP *********************************************************************
-localhost                  : ok=61   changed=0    unreachable=0    failed=1    skipped=68   rescued=0    ignored=0
-
-----------
-{"level":"error","ts":"2025-07-09T18:21:55Z","msg":"Reconciler error","controller":"awx-controller","object":{"name":"awx-ansible","namespace":"awx"},"namespace":"awx","name":"awx-ansible","reconcileID":"81f7aa03-c744-4db6-ac8a-99b78c656a7f","error":"event runner on failed","stacktrace":"sigs.k8s.io/controller-runtime/pkg/internal/controller.(*Controller).reconcileHandler\n\t/home/runner/go/pkg/mod/sigs.k8s.io/controller-runtime@v0.16.3/pkg/internal/controller/controller.go:329\nsigs.k8s.io/controller-runtime/pkg/internal/controller.(*Controller).processNextWorkItem\n\t/home/runner/go/pkg/mod/sigs.k8s.io/controller-runtime@v0.16.3/pkg/internal/controller/controller.go:266\nsigs.k8s.io/controller-runtime/pkg/internal/controller.(*Controller).Start.func2.2\n\t/home/runner/go/pkg/mod/sigs.k8s.io/controller-runtime@v0.16.3/pkg/internal/controller/controller.go:227"}
+kubectl get awx awx-ansible -n awx -o yaml | grep redis                                                                                                              ─╯
+      {"apiVersion":"awx.ansible.com/v1beta1","kind":"AWX","metadata":{"annotations":{},"name":"awx-ansible","namespace":"awx"},"spec":{"ingress_type":"none","postgres_storage_class":"managed-premium","redis_image":"redis","redis_image_version":"7.0","service_type":"ClusterIP","task_node_selector":"{\"workload\":\"awx\"}","task_tolerations":"[{\"key\":\"awx\",\"operator\":\"Equal\",\"value\":\"true\",\"effect\":\"NoSchedule\"}]","web_node_selector":"{\"workload\":\"awx\"}","web_tolerations":"[{\"key\":\"awx\",\"operator\":\"Equal\",\"value\":\"true\",\"effect\":\"NoSchedule\"}]"}}
+  redis_image: redis
+  redis_image_version: "7.0"

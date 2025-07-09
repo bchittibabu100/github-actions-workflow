b05a91b76ef4897fa281fc216fddda398c8d3b88
Chittibabu Boya <bchittibabu100@gmail.com>
2025-07-08 21:52:17 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,5 +1,31 @@
-kubectl get clusterrole | grep awx                                                                                                                                   ─╯
-awx-operator-metrics-reader                                            2025-07-09T04:28:40Z
-awx-operator-metrics-reader-ansible-awx-operator                       2025-07-08T02:03:10Z
-awx-operator-proxy-role                                                2025-07-09T04:28:40Z
-awx-operator-proxy-role-ansible-awx-operator                           2025-07-08T02:03:10Z
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: awx-operator-extra
+rules:
+- apiGroups: [""]
+  resources: ["secrets"]
+  verbs: ["create", "get", "list", "update", "patch", "delete"]
+
+kubectl apply -f awx-operator-extra-role.yaml
+
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: awx-operator-extra-binding
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: awx-operator-extra
+subjects:
+- kind: ServiceAccount
+  name: awx-operator-controller-manager
+  namespace: awx
+
+kubectl apply -f awx-operator-extra-binding.yaml
+
+kubectl auth can-i create secret --as=system:serviceaccount:awx:awx-operator-controller-manager
+kubectl delete pod -n awx -l control-plane=controller-manager
+
+kubectl delete awx awx-demo -n awx
+kubectl apply -f awx-deploy.yaml

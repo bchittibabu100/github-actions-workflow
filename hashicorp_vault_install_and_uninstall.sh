31796dc64a512c94e669822b61582bc20e4a32f2
Chittibabu Boya <bchittibabu100@gmail.com>
2025-07-14 13:42:25 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,86 +1,114 @@
-name: Deploy AWX
-
-on:
-  workflow_dispatch:
-    inputs:
-      environment:
-        description: "Environment name (e.g. dev, stg, prod)"
-        required: true
-        default: "dev"
-      cluster_name:
-        description: "AKS cluster name"
-        required: true
-
-env:
-  LOCATION: centralus
-  RESOURCE_GROUP: rg-${{ github.event.inputs.environment }}-hub-centralus
-  NAMESPACE: awx
-  AWX_REPO: https://github.com/ansible/awx-operator.git
-  AWX_TAG: 2.19.1
-  NODEPOOL_NAME: awxpool
-  NODEPOOL_VM_SIZE: Standard_D2ps_v6
-  TAINT_KEY: awx
-  TAINT_VALUE: true
-  TAINT_EFFECT: NoSchedule
-
-jobs:
-  create-nodepool:
-    name: Create AKS Node Pool
-    runs-on: ubuntu-latest
-
-    steps:
-      - name: Azure Login
-        uses: azure/login@v1
-        with:
-          creds: ${{ secrets.AZURE_CREDENTIALS }}
-
-      - name: Create AWX Node Pool with Taints
-        run: |
-          az aks nodepool add \
-            --resource-group $RESOURCE_GROUP \
-            --cluster-name ${{ github.event.inputs.cluster_name }} \
-            --name $NODEPOOL_NAME \
-            --node-count 1 \
-            --node-vm-size $NODEPOOL_VM_SIZE \
-            --labels workload=awx \
-            --aks-custom-headers UseGPUDedicatedVHD=true \
-            --node-taints $TAINT_KEY=$TAINT_VALUE:$TAINT_EFFECT \
-            --zones 1 \
-            --mode User
-
-  install-awx:
-    name: Deploy AWX to AKS
-    runs-on: ubuntu-latest
-    needs: create-nodepool
-
-    steps:
-      - name: Checkout AWX Installer
-        run: |
-          git clone --depth 1 --branch $AWX_TAG $AWX_REPO
-          cd awx-operator
-          echo "Cloned tag $AWX_TAG"
-
-      - name: Azure Login
-        uses: azure/login@v1
-        with:
-          creds: ${{ secrets.AZURE_CREDENTIALS }}
-
-      - name: Get AKS Credentials
-        run: |
-          az aks get-credentials \
-            --name ${{ github.event.inputs.cluster_name }} \
-            --resource-group $RESOURCE_GROUP \
-            --overwrite-existing
-
-      - name: Create Namespace
-        run: |
-          kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
-
-      - name: Deploy AWX Operator
-        run: |
-          cd awx-operator
-          make deploy NAMESPACE=$NAMESPACE
-
-      - name: Apply AWX Custom Resource
-        run: |
-          kubectl apply -f awx-deploy.yaml -n $NAMESPACE
+curl -v https://github.com > github_request.txt
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0*   Trying 140.82.112.3:443...
+* Connected to github.com (140.82.112.3) port 443 (#0)
+* ALPN, offering h2
+* ALPN, offering http/1.1
+*  CAfile: /etc/ssl/certs/ca-certificates.crt
+*  CApath: /etc/ssl/certs
+* TLSv1.0 (OUT), TLS header, Certificate Status (22):
+} [5 bytes data]
+* TLSv1.3 (OUT), TLS handshake, Client hello (1):
+} [512 bytes data]
+* TLSv1.2 (IN), TLS header, Certificate Status (22):
+{ [5 bytes data]
+* TLSv1.3 (IN), TLS handshake, Server hello (2):
+{ [122 bytes data]
+* TLSv1.2 (IN), TLS header, Finished (20):
+{ [5 bytes data]
+* TLSv1.2 (IN), TLS header, Supplemental data (23):
+{ [5 bytes data]
+* TLSv1.3 (IN), TLS handshake, Encrypted Extensions (8):
+{ [19 bytes data]
+* TLSv1.2 (IN), TLS header, Supplemental data (23):
+{ [5 bytes data]
+* TLSv1.3 (IN), TLS handshake, Certificate (11):
+{ [3135 bytes data]
+* TLSv1.2 (IN), TLS header, Supplemental data (23):
+{ [5 bytes data]
+* TLSv1.3 (IN), TLS handshake, CERT verify (15):
+{ [80 bytes data]
+* TLSv1.2 (IN), TLS header, Supplemental data (23):
+{ [5 bytes data]
+* TLSv1.3 (IN), TLS handshake, Finished (20):
+{ [36 bytes data]
+* TLSv1.2 (OUT), TLS header, Finished (20):
+} [5 bytes data]
+* TLSv1.3 (OUT), TLS change cipher, Change cipher spec (1):
+} [1 bytes data]
+* TLSv1.2 (OUT), TLS header, Supplemental data (23):
+} [5 bytes data]
+* TLSv1.3 (OUT), TLS handshake, Finished (20):
+} [36 bytes data]
+* SSL connection using TLSv1.3 / TLS_AES_128_GCM_SHA256
+* ALPN, server accepted to use h2
+* Server certificate:
+*  subject: CN=github.com
+*  start date: Feb  5 00:00:00 2025 GMT
+*  expire date: Feb  5 23:59:59 2026 GMT
+*  subjectAltName: host "github.com" matched cert's "github.com"
+*  issuer: C=GB; ST=Greater Manchester; L=Salford; O=Sectigo Limited; CN=Sectigo ECC Domain Validation Secure Server CA
+*  SSL certificate verify ok.
+* Using HTTP2, server supports multiplexing
+* Connection state changed (HTTP/2 confirmed)
+* Copying HTTP/2 data in stream buffer to connection buffer after upgrade: len=0
+* TLSv1.2 (OUT), TLS header, Supplemental data (23):
+} [5 bytes data]
+* TLSv1.2 (OUT), TLS header, Supplemental data (23):
+} [5 bytes data]
+* TLSv1.2 (OUT), TLS header, Supplemental data (23):
+} [5 bytes data]
+* Using Stream ID: 1 (easy handle 0x55c98ad039f0)
+* TLSv1.2 (OUT), TLS header, Supplemental data (23):
+} [5 bytes data]
+> GET / HTTP/2
+> Host: github.com
+> user-agent: curl/7.81.0
+> accept: */*
+>
+* TLSv1.2 (IN), TLS header, Supplemental data (23):
+{ [5 bytes data]
+* TLSv1.3 (IN), TLS handshake, Newsession Ticket (4):
+{ [57 bytes data]
+* TLSv1.2 (IN), TLS header, Supplemental data (23):
+{ [5 bytes data]
+* TLSv1.3 (IN), TLS handshake, Newsession Ticket (4):
+{ [57 bytes data]
+* old SSL session ID is stale, removing
+* TLSv1.2 (IN), TLS header, Supplemental data (23):
+{ [5 bytes data]
+* TLSv1.2 (OUT), TLS header, Supplemental data (23):
+} [5 bytes data]
+* TLSv1.2 (IN), TLS header, Supplemental data (23):
+{ [5 bytes data]
+* TLSv1.2 (IN), TLS header, Supplemental data (23):
+{ [5 bytes data]
+* TLSv1.2 (IN), TLS header, Supplemental data (23):
+{ [5 bytes data]
+* TLSv1.2 (IN), TLS header, Supplemental data (23):
+{ [5 bytes data]
+* TLSv1.2 (IN), TLS header, Supplemental data (23):
+{ [5 bytes data]
+< HTTP/2 200
+< date: Mon, 14 Jul 2025 20:36:03 GMT
+< content-type: text/html; charset=utf-8
+< vary: X-PJAX, X-PJAX-Container, Turbo-Visit, Turbo-Frame, X-Requested-With, Accept-Language,Accept-Encoding, Accept, X-Requested-With
+< content-language: en-US
+< etag: W/"e3c16e5eb2deb8bed5d9cb60dc27e404"
+< cache-control: max-age=0, private, must-revalidate
+< strict-transport-security: max-age=31536000; includeSubdomains; preload
+< x-frame-options: deny
+< x-content-type-options: nosniff
+< x-xss-protection: 0
+< referrer-policy: origin-when-cross-origin, strict-origin-when-cross-origin
+< content-security-policy: default-src 'none'; base-uri 'self'; child-src github.githubassets.com github.com/assets-cdn/worker/ github.com/assets/ gist.github.com/assets-cdn/worker/; connect-src 'self' uploads.github.com www.githubstatus.com collector.github.com raw.githubusercontent.com api.github.com github-cloud.s3.amazonaws.com github-production-repository-file-5c1aeb.s3.amazonaws.com github-production-upload-manifest-file-7fdce7.s3.amazonaws.com github-production-user-asset-6210df.s3.amazonaws.com *.rel.tunnels.api.visualstudio.com wss://*.rel.tunnels.api.visualstudio.com objects-origin.githubusercontent.com copilot-proxy.githubusercontent.com proxy.individual.githubcopilot.com proxy.business.githubcopilot.com proxy.enterprise.githubcopilot.com *.actions.githubusercontent.com wss://*.actions.githubusercontent.com productionresultssa0.blob.core.windows.net/ productionresultssa1.blob.core.windows.net/ productionresultssa2.blob.core.windows.net/ productionresultssa3.blob.core.windows.net/ productionresultssa4.blob.core.windows.net/ productionresultssa5.blob.core.windows.net/ productionresultssa6.blob.core.windows.net/ productionresultssa7.blob.core.windows.net/ productionresultssa8.blob.core.windows.net/ productionresultssa9.blob.core.windows.net/ productionresultssa10.blob.core.windows.net/ productionresultssa11.blob.core.windows.net/ productionresultssa12.blob.core.windows.net/ productionresultssa13.blob.core.windows.net/ productionresultssa14.blob.core.windows.net/ productionresultssa15.blob.core.windows.net/ productionresultssa16.blob.core.windows.net/ productionresultssa17.blob.core.windows.net/ productionresultssa18.blob.core.windows.net/ productionresultssa19.blob.core.windows.net/ github-production-repository-image-32fea6.s3.amazonaws.com github-production-release-asset-2e65be.s3.amazonaws.com insights.github.com wss://alive.github.com api.githubcopilot.com api.individual.githubcopilot.com api.business.githubcopilot.com api.enterprise.githubcopilot.com github.githubassets.com; font-src github.githubassets.com; form-action 'self' github.com gist.github.com copilot-workspace.githubnext.com objects-origin.githubusercontent.com; frame-ancestors 'none'; frame-src viewscreen.githubusercontent.com notebooks.githubusercontent.com www.youtube-nocookie.com; img-src 'self' data: blob: github.githubassets.com media.githubusercontent.com camo.githubusercontent.com identicons.github.com avatars.githubusercontent.com private-avatars.githubusercontent.com github-cloud.s3.amazonaws.com objects.githubusercontent.com release-assets.githubusercontent.com secured-user-images.githubusercontent.com/ user-images.githubusercontent.com/ private-user-images.githubusercontent.com opengraph.githubassets.com copilotprodattachments.blob.core.windows.net/github-production-copilot-attachments/ github-production-user-asset-6210df.s3.amazonaws.com customer-stories-feed.github.com spotlights-feed.github.com objects-origin.githubusercontent.com *.githubusercontent.com images.ctfassets.net/8aevphvgewt8/; manifest-src 'self'; media-src github.com user-images.githubusercontent.com/ secured-user-images.githubusercontent.com/ private-user-images.githubusercontent.com github-production-user-asset-6210df.s3.amazonaws.com gist.github.com github.githubassets.com assets.ctfassets.net/8aevphvgewt8/ videos.ctfassets.net/8aevphvgewt8/; script-src github.githubassets.com; style-src 'unsafe-inline' github.githubassets.com; upgrade-insecure-requests; worker-src github.githubassets.com github.com/assets-cdn/worker/ github.com/assets/ gist.github.com/assets-cdn/worker/
+< server: github.com
+< accept-ranges: bytes
+< set-cookie: _gh_sess=XMpd%2FLU%2FBeCKzj47vOhvoGJ%2FG75z3v%2Bg3mGipdqsWpobSFy4bYH0zUkfHlsSh3z328m365tleGvinc6b3K00OB0OCRES%2BupP5G8zCCISzNvm14ExDUySfMj3StxqUNCxRrgch4dJ1hSyJ6rN3hL%2BEAmZEdNlbTeHyFsRq2aaZYDLop1u8n7UJsmgrBxq9PzWghTpO88KgwfCQWBDJcbfMSoT2P4u8cKqpDeknEWzsCTEevHfS%2F4y5U9iAWDe0SnSZmcd6NShZfmsLuDaekeB0Q%3D%3D--gRPBkzaA4Nw%2BZqUr--ZmBbBfspsaXVsoNlPxCKKQ%3D%3D; Path=/; HttpOnly; Secure; SameSite=Lax
+< set-cookie: _octo=GH1.1.85323604.1752525371; Path=/; Domain=github.com; Expires=Tue, 14 Jul 2026 20:36:11 GMT; Secure; SameSite=Lax
+< set-cookie: logged_in=no; Path=/; Domain=github.com; Expires=Tue, 14 Jul 2026 20:36:11 GMT; HttpOnly; Secure; SameSite=Lax
+< x-github-request-id: B12C:1603CC:36ABDB:49EBD0:68756A3B
+<
+* TLSv1.2 (IN), TLS header, Supplemental data (23):
+{ [5 bytes data]

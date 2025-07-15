b6c0b9da0e337cadeee971b2604b32f59eee3fb4
Chittibabu Boya <bchittibabu100@gmail.com>
2025-07-15 10:45:15 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,114 +1,60 @@
-curl -v https://github.com > github_request.txt
-  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
-                                 Dload  Upload   Total   Spent    Left  Speed
-  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0*   Trying 140.82.112.3:443...
-* Connected to github.com (140.82.112.3) port 443 (#0)
-* ALPN, offering h2
-* ALPN, offering http/1.1
-*  CAfile: /etc/ssl/certs/ca-certificates.crt
-*  CApath: /etc/ssl/certs
-* TLSv1.0 (OUT), TLS header, Certificate Status (22):
-} [5 bytes data]
-* TLSv1.3 (OUT), TLS handshake, Client hello (1):
-} [512 bytes data]
-* TLSv1.2 (IN), TLS header, Certificate Status (22):
-{ [5 bytes data]
-* TLSv1.3 (IN), TLS handshake, Server hello (2):
-{ [122 bytes data]
-* TLSv1.2 (IN), TLS header, Finished (20):
-{ [5 bytes data]
-* TLSv1.2 (IN), TLS header, Supplemental data (23):
-{ [5 bytes data]
-* TLSv1.3 (IN), TLS handshake, Encrypted Extensions (8):
-{ [19 bytes data]
-* TLSv1.2 (IN), TLS header, Supplemental data (23):
-{ [5 bytes data]
-* TLSv1.3 (IN), TLS handshake, Certificate (11):
-{ [3135 bytes data]
-* TLSv1.2 (IN), TLS header, Supplemental data (23):
-{ [5 bytes data]
-* TLSv1.3 (IN), TLS handshake, CERT verify (15):
-{ [80 bytes data]
-* TLSv1.2 (IN), TLS header, Supplemental data (23):
-{ [5 bytes data]
-* TLSv1.3 (IN), TLS handshake, Finished (20):
-{ [36 bytes data]
-* TLSv1.2 (OUT), TLS header, Finished (20):
-} [5 bytes data]
-* TLSv1.3 (OUT), TLS change cipher, Change cipher spec (1):
-} [1 bytes data]
-* TLSv1.2 (OUT), TLS header, Supplemental data (23):
-} [5 bytes data]
-* TLSv1.3 (OUT), TLS handshake, Finished (20):
-} [36 bytes data]
-* SSL connection using TLSv1.3 / TLS_AES_128_GCM_SHA256
-* ALPN, server accepted to use h2
-* Server certificate:
-*  subject: CN=github.com
-*  start date: Feb  5 00:00:00 2025 GMT
-*  expire date: Feb  5 23:59:59 2026 GMT
-*  subjectAltName: host "github.com" matched cert's "github.com"
-*  issuer: C=GB; ST=Greater Manchester; L=Salford; O=Sectigo Limited; CN=Sectigo ECC Domain Validation Secure Server CA
-*  SSL certificate verify ok.
-* Using HTTP2, server supports multiplexing
-* Connection state changed (HTTP/2 confirmed)
-* Copying HTTP/2 data in stream buffer to connection buffer after upgrade: len=0
-* TLSv1.2 (OUT), TLS header, Supplemental data (23):
-} [5 bytes data]
-* TLSv1.2 (OUT), TLS header, Supplemental data (23):
-} [5 bytes data]
-* TLSv1.2 (OUT), TLS header, Supplemental data (23):
-} [5 bytes data]
-* Using Stream ID: 1 (easy handle 0x55c98ad039f0)
-* TLSv1.2 (OUT), TLS header, Supplemental data (23):
-} [5 bytes data]
-> GET / HTTP/2
-> Host: github.com
-> user-agent: curl/7.81.0
-> accept: */*
->
-* TLSv1.2 (IN), TLS header, Supplemental data (23):
-{ [5 bytes data]
-* TLSv1.3 (IN), TLS handshake, Newsession Ticket (4):
-{ [57 bytes data]
-* TLSv1.2 (IN), TLS header, Supplemental data (23):
-{ [5 bytes data]
-* TLSv1.3 (IN), TLS handshake, Newsession Ticket (4):
-{ [57 bytes data]
-* old SSL session ID is stale, removing
-* TLSv1.2 (IN), TLS header, Supplemental data (23):
-{ [5 bytes data]
-* TLSv1.2 (OUT), TLS header, Supplemental data (23):
-} [5 bytes data]
-* TLSv1.2 (IN), TLS header, Supplemental data (23):
-{ [5 bytes data]
-* TLSv1.2 (IN), TLS header, Supplemental data (23):
-{ [5 bytes data]
-* TLSv1.2 (IN), TLS header, Supplemental data (23):
-{ [5 bytes data]
-* TLSv1.2 (IN), TLS header, Supplemental data (23):
-{ [5 bytes data]
-* TLSv1.2 (IN), TLS header, Supplemental data (23):
-{ [5 bytes data]
-< HTTP/2 200
-< date: Mon, 14 Jul 2025 20:36:03 GMT
-< content-type: text/html; charset=utf-8
-< vary: X-PJAX, X-PJAX-Container, Turbo-Visit, Turbo-Frame, X-Requested-With, Accept-Language,Accept-Encoding, Accept, X-Requested-With
-< content-language: en-US
-< etag: W/"e3c16e5eb2deb8bed5d9cb60dc27e404"
-< cache-control: max-age=0, private, must-revalidate
-< strict-transport-security: max-age=31536000; includeSubdomains; preload
-< x-frame-options: deny
-< x-content-type-options: nosniff
-< x-xss-protection: 0
-< referrer-policy: origin-when-cross-origin, strict-origin-when-cross-origin
-< content-security-policy: default-src 'none'; base-uri 'self'; child-src github.githubassets.com github.com/assets-cdn/worker/ github.com/assets/ gist.github.com/assets-cdn/worker/; connect-src 'self' uploads.github.com www.githubstatus.com collector.github.com raw.githubusercontent.com api.github.com github-cloud.s3.amazonaws.com github-production-repository-file-5c1aeb.s3.amazonaws.com github-production-upload-manifest-file-7fdce7.s3.amazonaws.com github-production-user-asset-6210df.s3.amazonaws.com *.rel.tunnels.api.visualstudio.com wss://*.rel.tunnels.api.visualstudio.com objects-origin.githubusercontent.com copilot-proxy.githubusercontent.com proxy.individual.githubcopilot.com proxy.business.githubcopilot.com proxy.enterprise.githubcopilot.com *.actions.githubusercontent.com wss://*.actions.githubusercontent.com productionresultssa0.blob.core.windows.net/ productionresultssa1.blob.core.windows.net/ productionresultssa2.blob.core.windows.net/ productionresultssa3.blob.core.windows.net/ productionresultssa4.blob.core.windows.net/ productionresultssa5.blob.core.windows.net/ productionresultssa6.blob.core.windows.net/ productionresultssa7.blob.core.windows.net/ productionresultssa8.blob.core.windows.net/ productionresultssa9.blob.core.windows.net/ productionresultssa10.blob.core.windows.net/ productionresultssa11.blob.core.windows.net/ productionresultssa12.blob.core.windows.net/ productionresultssa13.blob.core.windows.net/ productionresultssa14.blob.core.windows.net/ productionresultssa15.blob.core.windows.net/ productionresultssa16.blob.core.windows.net/ productionresultssa17.blob.core.windows.net/ productionresultssa18.blob.core.windows.net/ productionresultssa19.blob.core.windows.net/ github-production-repository-image-32fea6.s3.amazonaws.com github-production-release-asset-2e65be.s3.amazonaws.com insights.github.com wss://alive.github.com api.githubcopilot.com api.individual.githubcopilot.com api.business.githubcopilot.com api.enterprise.githubcopilot.com github.githubassets.com; font-src github.githubassets.com; form-action 'self' github.com gist.github.com copilot-workspace.githubnext.com objects-origin.githubusercontent.com; frame-ancestors 'none'; frame-src viewscreen.githubusercontent.com notebooks.githubusercontent.com www.youtube-nocookie.com; img-src 'self' data: blob: github.githubassets.com media.githubusercontent.com camo.githubusercontent.com identicons.github.com avatars.githubusercontent.com private-avatars.githubusercontent.com github-cloud.s3.amazonaws.com objects.githubusercontent.com release-assets.githubusercontent.com secured-user-images.githubusercontent.com/ user-images.githubusercontent.com/ private-user-images.githubusercontent.com opengraph.githubassets.com copilotprodattachments.blob.core.windows.net/github-production-copilot-attachments/ github-production-user-asset-6210df.s3.amazonaws.com customer-stories-feed.github.com spotlights-feed.github.com objects-origin.githubusercontent.com *.githubusercontent.com images.ctfassets.net/8aevphvgewt8/; manifest-src 'self'; media-src github.com user-images.githubusercontent.com/ secured-user-images.githubusercontent.com/ private-user-images.githubusercontent.com github-production-user-asset-6210df.s3.amazonaws.com gist.github.com github.githubassets.com assets.ctfassets.net/8aevphvgewt8/ videos.ctfassets.net/8aevphvgewt8/; script-src github.githubassets.com; style-src 'unsafe-inline' github.githubassets.com; upgrade-insecure-requests; worker-src github.githubassets.com github.com/assets-cdn/worker/ github.com/assets/ gist.github.com/assets-cdn/worker/
-< server: github.com
-< accept-ranges: bytes
-< set-cookie: _gh_sess=XMpd%2FLU%2FBeCKzj47vOhvoGJ%2FG75z3v%2Bg3mGipdqsWpobSFy4bYH0zUkfHlsSh3z328m365tleGvinc6b3K00OB0OCRES%2BupP5G8zCCISzNvm14ExDUySfMj3StxqUNCxRrgch4dJ1hSyJ6rN3hL%2BEAmZEdNlbTeHyFsRq2aaZYDLop1u8n7UJsmgrBxq9PzWghTpO88KgwfCQWBDJcbfMSoT2P4u8cKqpDeknEWzsCTEevHfS%2F4y5U9iAWDe0SnSZmcd6NShZfmsLuDaekeB0Q%3D%3D--gRPBkzaA4Nw%2BZqUr--ZmBbBfspsaXVsoNlPxCKKQ%3D%3D; Path=/; HttpOnly; Secure; SameSite=Lax
-< set-cookie: _octo=GH1.1.85323604.1752525371; Path=/; Domain=github.com; Expires=Tue, 14 Jul 2026 20:36:11 GMT; Secure; SameSite=Lax
-< set-cookie: logged_in=no; Path=/; Domain=github.com; Expires=Tue, 14 Jul 2026 20:36:11 GMT; HttpOnly; Secure; SameSite=Lax
-< x-github-request-id: B12C:1603CC:36ABDB:49EBD0:68756A3B
-<
-* TLSv1.2 (IN), TLS header, Supplemental data (23):
-{ [5 bytes data]
+5 extracting sha256:0a9a5dfd008f05ebc27e4790db0709a29e527690c21bcbcd01481eaeb6bb49dc
+#5 extracting sha256:0a9a5dfd008f05ebc27e4790db0709a29e527690c21bcbcd01481eaeb6bb49dc 0.1s done
+#5 sha256:de4fe7064d8f98419ea6b49190df1abbf43450c1702eeb864fe9ced453c1cc5f 9.22kB / 9.22kB done
+#5 sha256:43180c492a5e6cedd8232e8f77a454f666f247586853eecb90258b26688ad1d3 1.02kB / 1.02kB done
+#5 sha256:ff221270b9fb7387b0ad9ff8f69fbbd841af263842e62217392f18c3b5226f38 581B / 581B done
+#5 sha256:0a9a5dfd008f05ebc27e4790db0709a29e527690c21bcbcd01481eaeb6bb49dc 3.63MB / 3.63MB 0.1s done
+#5 DONE 0.3s
+
+#6 [2/2] RUN apk add --no-cache       bash       jq       tzdata       fontconfig       liberation-fonts       zlib       icu-data-full       musl-locales
+#6 0.165 fetch https://dl-cdn.alpinelinux.org/alpine/v3.20/main/x86_64/APKINDEX.tar.gz
+#6 0.724 281B96E8FC7E0000:error:0A000086:SSL routines:tls_post_process_server_certificate:certificate verify failed:ssl/statem/statem_clnt.c:2103:
+#6 0.725 fetch https://dl-cdn.alpinelinux.org/alpine/v3.20/community/x86_64/APKINDEX.tar.gz
+#6 0.725 WARNING: fetching https://dl-cdn.alpinelinux.org/alpine/v3.20/main: Permission denied
+#6 0.759 281B96E8FC7E0000:error:0A000086:SSL routines:tls_post_process_server_certificate:certificate verify failed:ssl/statem/statem_clnt.c:2103:
+#6 0.760 WARNING: fetching https://dl-cdn.alpinelinux.org/alpine/v3.20/community: Permission denied
+#6 0.760 ERROR: unable to select packages:
+#6 0.760   bash (no such package):
+#6 0.760     required by: world[bash]
+#6 0.760   fontconfig (no such package):
+#6 0.760     required by: world[fontconfig]
+#6 0.760   icu-data-full (no such package):
+#6 0.760     required by: world[icu-data-full]
+#6 0.760   jq (no such package):
+#6 0.760     required by: world[jq]
+#6 0.760   liberation-fonts (no such package):
+#6 0.760     required by: world[liberation-fonts]
+#6 0.760   musl-locales (no such package):
+#6 0.760     required by: world[musl-locales]
+#6 0.760   tzdata (no such package):
+#6 0.760     required by: world[tzdata]
+#6 ERROR: process "/bin/sh -c apk add --no-cache       bash       jq       tzdata       fontconfig       liberation-fonts       zlib       icu-data-full       musl-locales" did not complete successfully: exit code: 7
+------
+ > [2/2] RUN apk add --no-cache       bash       jq       tzdata       fontconfig       liberation-fonts       zlib       icu-data-full       musl-locales:
+0.760   icu-data-full (no such package):
+0.760     required by: world[icu-data-full]
+0.760   jq (no such package):
+0.760     required by: world[jq]
+0.760   liberation-fonts (no such package):
+0.760     required by: world[liberation-fonts]
+0.760   musl-locales (no such package):
+0.760     required by: world[musl-locales]
+0.760   tzdata (no such package):
+0.760     required by: world[tzdata]
+------
+Dockerfile:4
+--------------------
+   3 |     
+   4 | >>> RUN apk add --no-cache \
+   5 | >>>       bash \
+   6 | >>>       jq \
+   7 | >>>       tzdata \
+   8 | >>>       fontconfig \
+   9 | >>>       liberation-fonts \
+  10 | >>>       zlib \
+  11 | >>>       icu-data-full \
+  12 | >>>       musl-locales
+  13 |     
+--------------------
+ERROR: failed to solve: process "/bin/sh -c apk add --no-cache       bash       jq       tzdata       fontconfig       liberation-fonts       zlib       icu-data-full       musl-locales" did not complete successfully: exit code: 7
+Error: Process completed with exit code 1.

d391f483ebf68372c29e62a87619274e0cb469bc
Chittibabu Boya <bchittibabu100@gmail.com>
2025-07-21 14:23:46 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,1 +1,9 @@
+sudo mkdir -p /etc/systemd/system/actions.runner.optum-financial.vpay-mo066inflrun01.service.d
+sudo vim /etc/systemd/system/actions.runner.optum-financial.vpay-mo066inflrun01.service.d/override.conf
+
+[Service]
 ExecStartPre=/bin/bash -c 'find /home/gitrunner/actions-runner/_work/_temp -user root -exec chown -R gitrunner:gitrunner {} +'
+
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl restart actions.runner.optum-financial.vpay-mo066inflrun01.service

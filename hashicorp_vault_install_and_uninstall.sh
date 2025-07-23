745b98b83626ce49bd5309b7ea5e5d0229d48ab3
Chittibabu Boya <bchittibabu100@gmail.com>
2025-07-23 06:13:34 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,25 +1,121 @@
-gitrunner@mo066inflrun05 ~/actions-runner/_work/vpay-rest-payments-api/vpay-rest-payments-api $ll
-total 104
-drwxr-xr-x 9 gitrunner gitrunner 4096 Jul 23 01:12 ./
-drwxr-xr-x 3 gitrunner gitrunner 4096 Jul  8 05:47 ../
-drwxr-xr-x 3 gitrunner gitrunner 4096 Jul 23 00:57 charts/
--rw-r--r-- 1 root      root        80 Jul 23 00:57 CODEOWNERS
--rw-r--r-- 1 root      root       809 Jul 23 00:57 docker-compose.dcproj
--rw-r--r-- 1 root      root       466 Jul 23 00:57 docker-compose.override.yml
--rw-r--r-- 1 root      root       170 Jul 23 00:57 docker-compose.yml
--rw-r--r-- 1 root      root      1967 Jul 23 01:12 Dockerfile
--rw-r--r-- 1 root      root       127 Jul 23 00:57 .dockerignore
--rw-r--r-- 1 root      root      7810 Jul 23 00:57 .editorconfig
-drwxr-xr-x 8 gitrunner gitrunner 4096 Jul 23 01:13 .git/
--rw-r--r-- 1 root      root      2564 Jul 23 00:57 .gitattributes
-drwxr-xr-x 4 gitrunner gitrunner 4096 Jul 23 00:57 .github/
--rw-r--r-- 1 root      root      4496 Jul 23 00:57 .gitignore
-drwxr-xr-x 2 gitrunner gitrunner 4096 Jul 23 00:57 .gitlab-ci/
--rw-r--r-- 1 root      root      1137 Jul 23 00:57 .gitlab-ci.yml
--rw-r--r-- 1 root      root       216 Jul 23 00:57 nuget.config
--rw-r--r-- 1 root      root       913 Jul 23 00:57 README.md
--rw-r--r-- 1 root      root      4255 Jul 23 00:57 RestPayments.sln
-drwxr-xr-x 3 gitrunner gitrunner 4096 Jul  8 05:48 scripts/
-drwxr-xr-x 3 gitrunner gitrunner 4096 Jul  8 05:48 src/
-drwxr-xr-x 3 gitrunner gitrunner 4096 Jul  8 05:48 tests/
--rw-r--r-- 1 root      root       312 Jul 23 00:57 vitals.yml
+name: build-and-publish
+
+on:
+  workflow_call:
+    secrets:
+      DOCKER_PASSWORD:
+        required: true
+    inputs:
+      DOCKER_URL:
+        required: true
+        type: string
+      DOCKER_USERNAME:
+        required: true
+        type: string
+      PROJECT:
+        required: true
+        type: string
+      IMAGE_NAME:
+        required: true
+        type: string
+      DOCKER_REGISTRY_URL:
+        required: false
+        type: string
+        default: plinfharbor.vayusa.net
+      DOCKERFILE:
+        required: false
+        type: string
+        default: Dockerfile
+      BUILD_ARGS:
+        required: false
+        type: string
+      BUILD_ARGS_ENV_FILE_PATH:
+        required: false
+        type: string
+      DOCKER_CONTEXT_DIR:
+        required: false
+        type: string
+        default: ""
+      PRE:
+        required: false
+        type: boolean
+        default: true
+      PREBUILD_COMMAND:
+        required: false
+        type: string
+
+env:
+  DOCKER_REGISTRY_URL: ${{inputs.DOCKER_REGISTRY_URL}}
+  PROJECT: ${{inputs.PROJECT}}
+  IMAGE_NAME: ${{inputs.IMAGE_NAME}}
+  DOCKERFILE: ${{inputs.DOCKERFILE}}
+  BUILD_ARGS: ${{inputs.BUILD_ARGS}}
+  DOCKER_CONTEXT_DIR: ${{inputs.DOCKER_CONTEXT_DIR}}
+  VERSION: ${{ github.event_name == 'pull_request' && github.event.pull_request.head.sha || github.ref_name }}
+
+jobs:
+  build-and-publish:
+    runs-on: uhg-runner
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v3
+        with:
+          fetch-depth: 0
+      - name: run prebuild command
+        run: ${{inputs.PREBUILD_COMMAND}}
+        if: inputs.PREBUILD_COMMAND
+        shell: bash
+      - name: set build args env variables
+        run: cat ${{inputs.BUILD_ARGS_ENV_FILE_PATH}}.env >> $GITHUB_ENV
+        if: inputs.BUILD_ARGS_ENV_FILE_PATH
+      - name: Login to Docker Container Registery
+        uses: docker/login-action@v3
+        with:
+          registry: ${{ inputs.DOCKER_URL }}
+          username: ${{ inputs.DOCKER_USERNAME }}
+          password: ${{ secrets.DOCKER_PASSWORD }}
+
+      - name: Build and Push Docker Image
+        run: |
+          echo "Building Docker image"
+          BUILD_IMAGE_NAME=${DOCKER_REGISTRY_URL}/${PROJECT}/${IMAGE_NAME}:${VERSION}
+          echo "BUILD_IMAGE_NAME=$BUILD_IMAGE_NAME" >> $GITHUB_ENV
+          echo "Generated image name: ${BUILD_IMAGE_NAME}"
+          echo "Retrieved build args: ${{ env.BUILD_ARGS }}"
+          if ! docker image pull ${BUILD_IMAGE_NAME} >/dev/null; then
+            set +x
+            BUILD_ARGS_FLAGS=${BUILD_ARGS:+$(printf -- "--build-arg %s " ${BUILD_ARGS[@]})}
+            echo "Generated build arg flags: ${BUILD_ARGS_FLAGS}"
+            echo "Building image: ${BUILD_IMAGE_NAME} in registry: ${DOCKER_REGISTRY_URL}"
+            docker image build -f ${DOCKERFILE} --tag ${BUILD_IMAGE_NAME} --build-arg REGISTRY=${DOCKER_REGISTRY_URL} ${BUILD_ARGS_FLAGS} --build-arg IMAGE_BUILD_TIME="$(TZ=UTC date -R)" ${DOCKER_CONTEXT_DIR:-${GITHUB_WORKSPACE}}
+            echo "Pushing image to ${DOCKER_REGISTRY_URL}"
+            docker image push ${BUILD_IMAGE_NAME}
+            set +x
+          else
+            echo "Image ${BUILD_IMAGE_NAME} already present"
+          fi
+        shell: bash
+      - name: Xray
+        uses: uhg-pipelines/epl-jf/xray-scan@v1
+        with:
+          type: image
+          score: 9.7
+          scan-type: xray
+          image: ${{ env.BUILD_IMAGE_NAME }}
+          user: ${{ inputs.DOCKER_USERNAME }}
+          registry-secret: ${{ secrets.DOCKER_PASSWORD }}
+        env:
+          DOCKER_REGISTRY_URL: ${{ inputs.DOCKER_REGISTRY_URL }}
+          PROJECT: ${{ inputs.PROJECT }}
+          IMAGE_NAME: ${{ inputs.IMAGE_NAME }}
+          DOCKERFILE: ${{ inputs.DOCKERFILE }}
+          BUILD_ARGS: ${{ inputs.BUILD_ARGS }}
+          DOCKER_CONTEXT_DIR: ${{ inputs.DOCKER_CONTEXT_DIR }}
+      - name: Promote
+        uses: chk-financial/payments-github/actions/promote@main
+        if: inputs.pre != true && !env.PROMOTED
+        with:
+          repository_name: vay-docker/${{ inputs.PROJECT }}/${{ inputs.IMAGE_NAME }}
+          tag: ${{ github.ref_name }}
+          username: ${{ inputs.DOCKER_USERNAME }}
+          password: ${{ secrets.DOCKER_PASSWORD }}

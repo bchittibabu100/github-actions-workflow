74a8c5bc28740763841120a751c0482aa60fa269
Chittibabu Boya <bchittibabu100@gmail.com>
2025-07-28 14:20:11 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,47 +1,27 @@
-name: List Azure Images
-on:
-  push:
-    branches:
-      - install-awx-installer-on-aks
+---
+- name: Check Vitals and Process Files
+  hosts: localhost
+  vars:
+    repo_name: "{{ lookup('env', 'repo_name') }}"
+    environment: "{{ ENVIRONMENT }}"
+    vault_environment: "{{ lookup('env', 'vault_environment') }}"
+    vpay_tpa_claims_api_url: "{{ lookup('env', 'VPAY_TPA_CLAIMS_API_URL') }}"
+    vpay_tpa_RestAPI: "{{ lookup('env', 'VPAY_TPA_REST_API_URL') }}"
+    vpay_tpa_mysqlhost: "{{ lookup('env', 'VPAY_TPA_MYSQLHOST') }}"
+    vpay_tpa_toemail: "{{ lookup('env', 'VPAY_TPA_TOEMAIL') }}"
+    vpay_tpa_fromemail: "{{ lookup('env', 'VPAY_TPA_FROMEMAIL') }}"
+    vpay_tpa_voidcheck: "{{ lookup('env', 'VPAY_TPA_VOIDCHECK') }}"
+    vpay_tpa_ftp: "{{ lookup('env', 'VPAY_TPA_FTPHOST') }}"
+    repo_variables_file: "{{ lookup('env', 'GITHUB_WORKSPACE') }}/actions-common/tpa/repo_variables.yaml"
+    repo_path: "{{ lookup('env', 'GITHUB_WORKSPACE') }}/{{ repo_name }}"
+    vitals_yaml_path: "{{ repo_path }}/vitals.yaml"
+    vitals_yml_path: "{{ repo_path }}/vitals.yml"
+  tasks:
 
-permissions:
-  id-token: write
-  contents: read
-
-jobs:
-  list-images:
-    runs-on: cg-runner
-    environment: eph
-
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@v3
-
-      - name: Check AZ
-        run: az version
-
-      - name: Azure login
-        uses: azure/login@v2
-        with:
-          tenant-id: ${{ vars.PMNTS_AZURE_TENANT_ID }}
-          client-id: ${{ vars.OPAY_AZURE_CLIENT_ID }}
-          subscription-id: ${{ vars.OPAY_AZURE_SUBSCRIPTION_ID }}
-
-      - name: List Azure Images
-        uses: azure/cli@v2
-        with:
-          azcliversion: latest
-          inlineScript: |
-            az account show
-            az sig image-version list --resource-group prod_golden_image_azu_gallery --gallery-name prod_golden_image_azu_gallery --gallery-image-definition RHEL_9 --output table
-
-
-logs:
-Run azure/cli@v2
-  
-Starting script execution via docker image mcr.microsoft.com/azure-cli:latest
-ERROR: Please run 'az login' to setup account.
-Error: Error: az cli script failed.
-cleaning up container...
-MICROSOFT_AZURE_CLI_1753419720190_CONTAINER
-Error: az cli script failed.
+    - name: Check if vitals.yaml or vitals.yml exists
+      stat:
+        path: "{{ item }}"
+      loop:
+        - "{{ vitals_yaml_path }}"
+        - "{{ vitals_yml_path }}"
+      register: vitals_files

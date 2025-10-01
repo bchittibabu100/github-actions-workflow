7dcc26a31b12d3a350ca59225a69c2cef208a3a5
Chittibabu Boya <bchittibabu100@gmail.com>
2025-09-30 18:24:00 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,3 +1,9 @@
-https://www.youtube.com/watch?v=H3PhTw0AnUA
-https://www.youtube.com/watch?v=lMRcUWny8E8
-https://www.youtube.com/watch?v=Acm18rVby3s
+Here is the portion of my config.ini file will look like and would like to replace the value by fetching from hashicorp vault using ansible playbook. Give me playbook which works with this template.
+
+
+[CheckNumberDatabase]
+# `Vault Helper` deployment task in bamboo will replace anything inside of double curly braces in any text file that is targeted by it.
+hostname={{ vault "dba/Accounts/Application/stage/PLATFORM/OCFParser" "hostname" }}
+password={{ vault "dba/Accounts/Application/stage/PLATFORM/OCFParser" "password" }}
+username={{ vault "dba/Accounts/Application/stage/PLATFORM/OCFParser" "username" }}
+database={{ vault "dba/Accounts/Application/stage/PLATFORM/OCFParser" "database" }}

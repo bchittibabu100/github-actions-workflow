d0903381c75b21043ea21233d2ceef844fe93917
Chittibabu Boya <bchittibabu100@gmail.com>
2025-10-22 19:19:02 -0700

    Update hashicorp_vault_install_and_uninstall.sh

--- hashicorp_vault_install_and_uninstall.sh
+++ hashicorp_vault_install_and_uninstall.sh
@@ -1,127 +1,87 @@
-Getting generic meesage of "The secret 'test' doesn't seem to exist.. The secret 'test' doesn't seem to exist". Secret exist and able to fetch it using python script with hvac module.
-Does playbook looks ok ?
+#######################################
+## This Dockerfile requires BuildKit ##
+#######################################
 
-Playbook:
-- name: Fetch Vault secret using mount_point
-  hosts: localhost
-  gather_facts: false
+## General arguments
+ARG REGISTRY=jfrog.vpayusa.net/plinfharbor
+ARG DOTNET_VERSION=6.0
+ARG DOTNET_SDK_VARIANT=commit-master
+ARG DOTNET_RUNTIME_VARIANT=commit-master
+ARG BASE_SDK_IMAGE=dotnet-bogner-sdk
+ARG BASE_RUNTIME_IMAGE=dotnet-bogner-aspnetcore
 
-  tasks:
-    - name: Get password from Vault
-      ansible.builtin.set_fact:
-        vault_password: >-
-          {{ lookup(
-            'community.hashi_vault.hashi_vault',
-            'test',
-            kv_version=2,
-            mount_point='dba',
-            url='http://10.130.112.100:8200',
-            token='hvs.qnno8Z432aoJ24',
-            key='password'
-          ) }}
+## Build Stage
+FROM ${REGISTRY}/base-images/${BASE_SDK_IMAGE}-${DOTNET_VERSION}:${DOTNET_SDK_VARIANT} as build
 
-    - name: Show password
-      ansible.builtin.debug:
-        msg: "Vault password is: {{ vault_password }}"
+## Build stage arguments
+ARG CONFIG_PROFILE=Release
+ARG PROJECT_DIR
+ARG PROJECT_NAME
 
-Logs:
-mo066inflrun01 test_ansible # ansible-galaxy collection install community.hashi_vault --force
-Starting galaxy collection install process
-Process install dependency map
-Starting collection install process
-Downloading https://galaxy.ansible.com/api/v3/plugin/ansible/content/published/collections/artifacts/community-hashi_vault-7.0.0.tar.gz to /root/.ansible/tmp/ansible-local-419839jj4r7b_1/tmppwlp3_t9/community-hashi_vault-7.0.0-weijfb9q
-Installing 'community.hashi_vault:7.0.0' to '/root/.ansible/collections/ansible_collections/community/hashi_vault'
-community.hashi_vault:7.0.0 was installed successfully
-mo066inflrun01 test_ansible # ansible-playbook vault_access_test.yaml -vvv
-ansible-playbook [core 2.17.14]
-  config file = None
-  configured module search path = ['/root/.ansible/plugins/modules', '/usr/share/ansible/plugins/modules']
-  ansible python module location = /usr/local/lib/python3.10/dist-packages/ansible
-  ansible collection location = /root/.ansible/collections:/usr/share/ansible/collections
-  executable location = /usr/local/bin/ansible-playbook
-  python version = 3.10.12 (main, Aug 15 2025, 14:32:43) [GCC 11.4.0] (/usr/bin/python3)
-  jinja version = 3.0.3
-  libyaml = True
-No config file found; using defaults
-host_list declined parsing /etc/ansible/hosts as it did not pass its verify_file() method
-Skipping due to inventory source not existing or not being readable by the current user
-script declined parsing /etc/ansible/hosts as it did not pass its verify_file() method
-auto declined parsing /etc/ansible/hosts as it did not pass its verify_file() method
-Skipping due to inventory source not existing or not being readable by the current user
-yaml declined parsing /etc/ansible/hosts as it did not pass its verify_file() method
-Skipping due to inventory source not existing or not being readable by the current user
-ini declined parsing /etc/ansible/hosts as it did not pass its verify_file() method
-Skipping due to inventory source not existing or not being readable by the current user
-toml declined parsing /etc/ansible/hosts as it did not pass its verify_file() method
-[WARNING]: No inventory was parsed, only implicit localhost is available
-[WARNING]: provided hosts list is empty, only localhost is available. Note that the implicit localhost does not match 'all'
-Skipping callback 'default', as we already have a stdout callback.
-Skipping callback 'minimal', as we already have a stdout callback.
-Skipping callback 'oneline', as we already have a stdout callback.
+ENV PROJECT=${PROJECT_DIR}/${PROJECT_NAME}.csproj
+WORKDIR /app
 
-PLAYBOOK: vault_access_test.yaml ******************************************************************************************************************************************
-1 plays in vault_access_test.yaml
+COPY nuget.config* ./
+COPY *.sln ./
 
-PLAY [Test Vault access using runtime lookup] *****************************************************************************************************************************
+## Copy .csproj files into the correct file structure
+SHELL ["/bin/bash", "-O", "globstar", "-c"]
+RUN --mount=target=docker_build_context \
+cd docker_build_context;\
+cp **/*.csproj ../ --parents;
+RUN rm -rf docker_build_context
+SHELL ["/bin/sh", "-c"]
 
-TASK [Fetch Vault secret into a variable] *********************************************************************************************************************************
-task path: /home/cboya1/test_ansible/vault_access_test.yaml:7
-exception during Jinja2 execution: Traceback (most recent call last):
-  File "/usr/local/lib/python3.10/dist-packages/ansible/template/__init__.py", line 827, in _lookup
-    ran = instance.run(loop_terms, variables=self._available_variables, **kwargs)
-  File "/root/.ansible/collections/ansible_collections/community/hashi_vault/plugins/lookup/hashi_vault.py", line 269, in run
-    ret.extend(self.get())
-  File "/root/.ansible/collections/ansible_collections/community/hashi_vault/plugins/lookup/hashi_vault.py", line 316, in get
-    raise AnsibleError("The secret '%s' doesn't seem to exist." % secret)
-ansible.errors.AnsibleError: The secret 'test' doesn't seem to exist.
-fatal: [localhost]: FAILED! => {
-    "msg": "An unhandled exception occurred while running the lookup plugin 'community.hashi_vault.hashi_vault'. Error was a <class 'ansible.errors.AnsibleError'>, original message: The secret 'test' doesn't seem to exist.. The secret 'test' doesn't seem to exist."
-}
+## Restore project
+RUN dotnet restore ${PROJECT}
+## Copy all files if restore succeeds
+COPY . ./
+## Publish project without restoring
+RUN dotnet publish --no-restore -c ${CONFIG_PROFILE} -o /app/out ${PROJECT}
 
-PLAY RECAP ****************************************************************************************************************************************************************
-localhost                  : ok=0    changed=0    unreachable=0    failed=1    skipped=0    rescued=0    ignored=0
+## New stage used to reduce the size of the final image
+FROM $REGISTRY/base-images/${BASE_RUNTIME_IMAGE}-${DOTNET_VERSION}:${DOTNET_RUNTIME_VARIANT} AS final
+## Final stage arguments
+ARG PROJECT_NAME
 
+COPY --chown=bogner:bogner src/requirements_py3.txt /
+COPY --chown=bogner:bogner lib/*.deb .
 
-Here is the python script that works.
+# TODO: split into Dockerfile.bogner.python
+RUN apt-get update \
+    && ACCEPT_EULA=y apt-get install -y --no-install-recommends \
+        default-jre-headless \
+        "g++" \
+        jq \
+        libbcprov-java \
+        libcommons-lang3-java \
+        libmysqlclient-dev \
+        msodbcsql18 \
+        python3-dev \
+        python3-pip \
+    && dpkg -i ./*.deb \
+    && python3 -m pip install --upgrade \
+        pip \
+        "setuptools<58" \
+        wheel \
+    && python3 -m pip install --upgrade -r /requirements_py3.txt \
+    && rm -rf /var/cache/apk/* \
+    && rm -rf /var/lib/apt/lists/* \
+    && rm -f ./*.deb
 
-import hvac
-import configparser
+WORKDIR /app
 
-# Vault connection parameters
-vault_url = "http://10.130.112.100:8200"
-vault_token = "hvs.qnno8Z432aoJ24"
-mount_point = "dba"
-secret_path = "test"
+COPY --from=build --chown=bogner:bogner /app/out .
+ENV ASPNETCORE_URLS=http://+:8080
 
-# Create a Vault client
-client = hvac.Client(url=vault_url, token=vault_token)
+## Create a symlink so we can use exec form entrypoint
+RUN ln -s ${PROJECT_NAME}.dll Entrypoint.dll
 
-# Check if the client is authenticated
-if client.is_authenticated():
-    try:
-        # Read the secret from KV v2
-        response = client.secrets.kv.v2.read_secret_version(
-            path=secret_path,
-            mount_point=mount_point
-        )
-        secret_data = response['data']['data']
+# Changing user after creating symbolic link to ensure user has permission to create that link
+USER bogner:bogner
 
-        # Create a configparser object and populate it
-        config = configparser.ConfigParser()
-        config['vault_secret'] = secret_data
+ENTRYPOINT [ "dotnet", "Entrypoint.dll" ]
 
-        # Write to config.ini
-        with open('config.ini', 'w') as configfile:
-            config.write(configfile)
-
-        print("Secrets successfully written to config.ini")
-    except Exception as e:
-        print(f"Error retrieving secret: {e}")
-else:
-    print("Vault authentication failed.")
-
-Execution logs:
-python3 vault_test.py
-/home/cboya1/test_ansible/vault_test.py:17: DeprecationWarning: The raise_on_deleted_version parameter will change its default value to False in hvac v3.0.0. The current default of True will presere previous behavior. To use the old behavior with no warning, explicitly set this value to True. See https://github.com/hvac/hvac/pull/907
-  response = client.secrets.kv.v2.read_secret_version(
-Retrieved password: hello123
+## Optionally add image build time
+ARG IMAGE_BUILD_TIME
+ENV IMAGE_BUILD_TIME ${IMAGE_BUILD_TIME}
